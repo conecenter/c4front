926d1c9cddefe7c29facc669802b7a0660e1292a
Sergei Kirjanov <rewlad@gmail.com>
2021-01-06 17:05:37 +0200

    taken from c4proto

---
+++ src/main/vdom-util.js
@@ -0,0 +1,18 @@
+
+const spreadAll = (...args) => Object.assign({},...args)
+
+export const deleted = ks => st => spreadAll(...Object.keys(st).filter(ck=>!ks[ck]).map(ck=>({[ck]:st[ck]})))
+
+export const weakCache = f => {
+    const map = new WeakMap
+    return arg => {
+        if(map.has(arg)) return map.get(arg)
+        const res = f(arg)
+        map.set(arg,res)
+        return res
+    }
+}
+
+export const identityAt = key => weakCache(parent => ({ parent, key }))
+
+export const never = o => { console.log(o); throw new Error }
