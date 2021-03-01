7239fc825a51fd5eb9a9aeaac6bee73821eebb27
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-03-01 17:09:03 +0200

    double click handler

--- src/main/vdom-pivot.js
+++ src/main/vdom-pivot.js
@@ -35,15 +35,20 @@ export function PivotRoot({rows, cols, children, classNames: argClassNames}) {
 
 const clickActionIdOf = identityAt('clickAction')
 
+//On handling double click https://medium.com/trabe/prevent-click-events-on-double-click-with-react-with-and-without-hooks-6bf3697abc40
 export function PivotCell({identity, colKey, rowKey, classNames, children}) {
     const className = classNames ? classNames.join(" ") : ""
     const gridArea = `${fromKey(rowKey)} / ${fromKey(colKey)} / ${toKey(rowKey)} / ${toKey(colKey)}`
     const [clickActionPatches, enqueueClickActionPatch] = useSync(clickActionIdOf(identity))
     const onClick = useCallback(ev => {
-        enqueueClickActionPatch({})
-        ev.stopPropagation()
+        enqueueClickActionPatch({headers: {"x-r-action": "click"}});
+        ev.stopPropagation();
     }, [enqueueClickActionPatch])
-    return $("div", {style: {gridArea}, className, children, onClick})
+    const onDoubleClick = useCallback(ev => {
+        enqueueClickActionPatch({headers: {"x-r-action": "double-click"}});
+        ev.stopPropagation();
+    }, [enqueueClickActionPatch])
+    return $("div", {style: {gridArea}, className, children, onClick, onDoubleClick})
 }
 
 export const components = {PivotRoot,PivotCell}
