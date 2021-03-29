0d7318786d4bd7a3ac00974ad50758915971df7d
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-03-29 17:58:49 +0300

    save

--- src/sapi/ee/cone/c4ui/DatePickerApi.scala
+++ src/sapi/ee/cone/c4ui/DatePickerApi.scala
@@ -5,6 +5,21 @@ import ee.cone.c4vdom.Types._
 import ee.cone.c4vdom._
 
 trait DatePicker extends ToChildPair
+
+@c4tagSwitch("FrontApp") trait Locale extends ToJson
+
+@c4tagSwitch("FrontApp") trait WeekDay extends ToJson {
+  def id: Int
+}
+
+@c4tagSwitch("FrontApp") trait Month extends ToJson {
+  def id: Int
+}
+
+@c4tagSwitch("FrontApp") trait DateTimeFormat extends ToJson {
+  def id: Int
+}
+
 @c4tagSwitch("FrontApp") trait DatePickerState extends ToJson
 
 @c4tags("FrontApp") trait DatePickerTags[C] {
@@ -25,4 +40,45 @@ trait DatePicker extends ToChildPair
     inputValue: String,
     tempTimestamp: Long = 0L
   ): DatePickerState
+
+  @c4el("UserLocaleProvider") def localeProvider(
+    key: String,
+    locale: Locale,
+    children: ChildPairList[OfDiv] = Nil,
+  ): ToChildPair
+}
+
+@c4tags("FrontApp") trait LocaleTags {
+  @c4val def locale(
+    timezoneId: String,
+    shortName: String,
+    weekDays: List[WeekDay],
+    months: List[Month],
+    dateTimeFormats: List[DateTimeFormat],
+    defaultDateFormatId: Int
+  ): Locale
+
+  @c4val def weekDay(
+    id: Int,
+    shortName: String,
+    fullName: String,
+  ): WeekDay
+
+  @c4val def month(
+    id: Int,
+    shortName: String,
+    fullName: String,
+  ): Month
+
+  @c4val def dateTimeFormat(
+    id: Int,
+    dateSeparator: String,
+    d: Int,
+    M: Int,
+    y: Int,
+    H: Int,
+    m: Int,
+    s: Int,
+    S: Int
+  ): DateTimeFormat
 }
