346b940716fcde8ec30cab1e41fc6c0eb3389dab
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-04-07 17:15:04 +0300

    save

--- src/main/keyboard-keys.ts
+++ src/main/keyboard-keys.ts
@@ -9,3 +9,4 @@ export const ARROW_DOWN_KEY: Key = {code: "ArrowDown"}
 export const ARROW_LEFT_KEY: Key = {code: "ArrowLeft"}
 export const ARROW_RIGHT_KEY: Key = {code: "ArrowRight"}
 export const ENTER_KEY: Key = {code: "Enter"}
+export const M_KEY: Key = {code: "KeyM"}
