dfe5a3629ac97f709000eb7eb517a777fd003596
Sergei Kirjanov <rewlad@gmail.com>
2021-07-23 19:30:35 +0300

    fix

--- src/sapi/ee/cone/c4ui/DictApi.scala
+++ src/sapi/ee/cone/c4ui/DictApi.scala
@@ -4,7 +4,7 @@ import ee.cone.c4di._
 import ee.cone.c4vdom.Types._
 import ee.cone.c4vdom._
 
-class DictApi {
+@c4tags("FrontApp") trait DictTags {
   @c4el("Dict") def dict(
     key: String,
     url: String,
