3db571d93d48bfebe1e718ad33b5087a0dc122c3
Aleafan <ae.afanasov@yandex.com>
2021-12-07 19:22:45 +0300

    *correct daysPrevMonth logic if month start with Monday *add toggle popup logic on click outside *add basic <select> with months

--- src/extra/datepicker/datepicker-calendar.tsx
+++ src/extra/datepicker/datepicker-calendar.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useUserLocale } from "../locale";
-import { endOfYear, getDate, getDaysInMonth, getWeek, isMonday, startOfWeek } from "date-fns";
-import { subWeeks } from "date-fns/esm";
-import { PopupDate } from "./datepicker";
+import { getDate, getDaysInMonth, getWeek, isMonday, startOfWeek } from "date-fns";
+import { useEventListener } from '../../main/vdom-hooks'
 
 interface DatepickerCalendarProps {
-  popupDate: { year: number, month: number }
+  popupDate: { year: number, month: number },
+  onClickAway: () => void
 }
 
-export function DatepickerCalendar({ popupDate }: DatepickerCalendarProps) {
+export function DatepickerCalendar({ popupDate, onClickAway }: DatepickerCalendarProps) {
   const { year, month } = popupDate;
 
   const pageDate = new Date(year, month);
@@ -16,27 +16,15 @@ export function DatepickerCalendar({ popupDate }: DatepickerCalendarProps) {
 
   const locale = useUserLocale();
   const pageMonth = locale.getMonthNameFull(month);
-
-  let fromDatePrevMonth: Date;
-  let startIndex: number, endIndex: number;
   
-  if (isMonday(pageDate)) {
-    fromDatePrevMonth = subWeeks(pageDate, 1);
-    startIndex = getDate(fromDatePrevMonth);
-    endIndex = startIndex + 6;
-  } else {
-    fromDatePrevMonth = startOfWeek(pageDate, { weekStartsOn: 1 });
-    startIndex = getDate(fromDatePrevMonth);
-    endIndex = getDaysInMonth(fromDatePrevMonth);
-  }
-  const daysPrevMonth = getSpanList(createArray(startIndex, endIndex), 'prev', 'dayPrevMonth');
+  const daysPrevMonth = isMonday(pageDate) ? [] : calcDaysPrevMonth(pageDate);
 
   const daysCurrMonth = getSpanList(createArray(1, getDaysInMonth(pageDate)), 'curr');
 
   const numDaysNextMonth = weeksToShow * 7 - daysPrevMonth.length - daysCurrMonth.length;
   const daysNextMonth = getSpanList(createArray(1, numDaysNextMonth), 'next', 'dayNextMonth');  
 
-  const weekNumStart = getWeek(fromDatePrevMonth, { weekStartsOn: 1, firstWeekContainsDate: 4 });
+  const weekNumStart = getWeek(pageDate, { weekStartsOn: 1, firstWeekContainsDate: 4 });
   const weekNumEnd = weekNumStart + weeksToShow - 1;
 
   const weekNumbersArr = (month === 0 && weekNumStart !== 1)
@@ -49,9 +37,25 @@ export function DatepickerCalendar({ popupDate }: DatepickerCalendarProps) {
   const weekDays = locale.weekDays
     .map(({ shortName }) => <span key={`${shortName}`}>{shortName}</span>);
 
+  const dpCalendar = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    function handleClick (e: MouseEvent) {
+      if (dpCalendar.current && !dpCalendar.current.contains(e.target as Node)) onClickAway();
+    }
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, []);
+
   return (
-    <div className='dpCalendar'>
-      <h2>{pageMonth} {year}</h2>
+    <div ref={dpCalendar} className='dpCalendar'>
+      <h2>
+        <select defaultValue={month}>
+          {locale.months.map(monthName => 
+            <option key={`month-${monthName.id}`} value={monthName.id}>{monthName.fullName}</option>
+          )}
+        </select>
+        {year}
+      </h2>
       <div className='dpCalendarContainer'>
         <div className='dpCalendarWeekDays'>
           {weekDays}
@@ -75,4 +79,11 @@ function createArray(start: number, end: number) {
 
 function getSpanList(array: number[], keyPrefix: string, className?: string) {
   return array.map(number => <span className={className} key={`${keyPrefix}${number}`}>{number}</span>);
+}
+
+function calcDaysPrevMonth(pageDate: Date) {
+  const firstWeekStart = startOfWeek(pageDate, { weekStartsOn: 1 });
+  const startIndex = getDate(firstWeekStart);
+  const endIndex = getDaysInMonth(firstWeekStart);
+  return getSpanList(createArray(startIndex, endIndex), 'prev', 'dayPrevMonth');
 }
