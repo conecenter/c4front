828921201068e6e0df589b83cfd171d1d8340182
Aleafan <ae.afanasov@yandex.com>
2021-12-14 10:50:31 +0200

    add config for HMR work in Windows

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        watch: {
+          usePolling: true
+        }
+      }
 })
