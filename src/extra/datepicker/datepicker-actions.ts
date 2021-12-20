a50aca7b0f38ad86ae474760c5cdffdee6c69fe4
Aleafan <ae.afanasov@yandex.com>
2021-12-20 20:02:50 +0300

    move to datepicker-calendar.tsx related functions

--- src/extra/datepicker/datepicker-actions.ts
+++ src/extra/datepicker/datepicker-actions.ts
@@ -1,9 +1,8 @@
-import {DateSettings, incrementDate, parseStringToDate, getTimestamp, getDate} from "./date-utils";
+import {DateSettings, incrementDate, parseStringToDate, getDate} from "./date-utils";
 import {createInputState, createTimestampState, DatePickerState, isTimestampState, PopupDate} from "./datepicker-exchange";
-import {getOrElse, isEmpty, mapOption, None, nonEmpty, Option} from "../../main/option";
-import React, {ChangeEvent, KeyboardEvent, MouseEvent} from "react";
+import {getOrElse, mapOption, None, nonEmpty, Option} from "../../main/option";
+import React, {ChangeEvent, KeyboardEvent} from "react";
 import {ARROW_DOWN_KEY, ARROW_UP_KEY, ENTER_KEY} from "../../main/keyboard-keys";
-import {addMonths, set} from "date-fns";
 
 function updateAndSendDate(
     currentDate: Date,
@@ -91,82 +90,16 @@ function getOnPopupToggle(
             popupDate = { year: currentDateOpt.getFullYear(), month: currentDateOpt.getMonth() };
         } else {
             const today =  getDate(Date.now(), dateSettings);
-            popupDate = nonEmpty(today) ? { year: today.getFullYear(), month: today.getMonth() } : undefined;
+            popupDate = nonEmpty(today) ? { year: today.getFullYear(), month: today.getMonth() } : None;
         }
         setState({ ...currentState, popupDate });
     }
 }
-
-function getOnDateChoice(
-    currentDateOpt: Option<Date>,
-    dateSettings: DateSettings,
-    setFinalState: (state: DatePickerState) => void) {
-    return (e: MouseEvent) => {
-        if (!(e.target instanceof HTMLSpanElement && e.target.dataset.date)) return;
-        const dateString = e.target.dataset.date;
-        const dateValues = dateString.split('-');
-        const isDateAvailable = nonEmpty(currentDateOpt);
-        const baseDate = isDateAvailable ? currentDateOpt : getDate(Date.now(), dateSettings);
-        if (isEmpty(baseDate)) return;
-        const timeSettings = isDateAvailable ? {} : { hours: 0, minutes: 0, seconds: 0 };
-        const chosenDate = set(baseDate, {
-            year: +dateValues[2],
-            month: +dateValues[1],
-            date: +dateValues[0],
-            ...timeSettings
-        });
-        setFinalState(createTimestampState(getTimestamp(chosenDate, dateSettings), None));
-    }
-}
-
-function getOnMonthArrowClick(currentState: DatePickerState, setFinalState: (state: DatePickerState) => void) {
-    return (e: MouseEvent) => {
-        const change = e.target instanceof HTMLButtonElement ? e.target.dataset.change : undefined;
-        if (change && nonEmpty(currentState.popupDate)) {
-            const { year, month } = currentState.popupDate!;
-            const newDate = addMonths(new Date(year, month), +change);
-            setFinalState({...currentState, popupDate: { year: newDate.getFullYear(), month: newDate.getMonth() }});
-        }
-    }
-};
-
-function getOnNowBtnClick(setFinalState: (state: DatePickerState) => void) {
-    return () => setFinalState(createTimestampState(Date.now(), None));
-}
-
- function getOnClearBtnClick(setFinalState: (state: DatePickerState) => void) {
-     return () => setFinalState(createInputState(''));
- }
-
- function getOnToggleMonthPopup(setMonthPopupShow: React.Dispatch<React.SetStateAction<boolean>>) {
-     return () => setMonthPopupShow(prevMonthPopup => !prevMonthPopup);
- }
-
- function getOnMonthPopupMiss(element: HTMLDivElement | null, setMonthPopupShow: React.Dispatch<React.SetStateAction<boolean>>) {
-     return (e: React.MouseEvent<HTMLElement>) => {
-        if (element && !element.contains(e.target as Node) && (<HTMLButtonElement>e.target).id !== 'btnDpMonthPopup') setMonthPopupShow(false);
-      }
- }
-
- function getOnMonthChoice(currentState: DatePickerState, setFinalState: (state: DatePickerState) => void, setMonthPopupShow: React.Dispatch<React.SetStateAction<boolean>>) {
-    return (e: MouseEvent) => {
-        const newPopupDate = { year: currentState.popupDate!.year, month: +e.target.dataset.month };
-        setMonthPopupShow(false);
-        setFinalState({...currentState, popupDate: newPopupDate });
-    }
- }
 
 export { 
     onTimestampChangeAction, 
     getOnKeyDown, 
     getOnChange, 
     getOnBlur, 
-    getOnPopupToggle, 
-    getOnDateChoice,
-    getOnMonthArrowClick,
-    getOnNowBtnClick,
-    getOnClearBtnClick,
-    getOnToggleMonthPopup,
-    getOnMonthPopupMiss,
-    getOnMonthChoice
+    getOnPopupToggle
 };
