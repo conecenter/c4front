b2277db82e59d9f153d097ad649f5168886b4c38
Aleafan <ae.afanasov@yandex.com>
2022-02-05 17:16:35 +0300

    *correct bug with left: 0 *add minWidth *create common-types file

---
+++ src/extra/common-types.ts
@@ -0,0 +1,10 @@
+interface PopupPosition { 
+    position: 'fixed', 
+    top: number,
+    left: number, 
+    visibility?: 'hidden', 
+    width?: number, 
+    minWidth?: number
+};
+
+export type { PopupPosition };
