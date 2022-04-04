df9c798485d4a8c60d143244088b9c75016dea74
Ilya Halsky <ilya.kachalsky@gmail.com>
2022-04-04 14:56:10 +0300

    clickable InlineElement

--- src/sapi/ee/cone/c4ui/InlineElementsApi.scala
+++ src/sapi/ee/cone/c4ui/InlineElementsApi.scala
@@ -1,31 +1,23 @@
 package ee.cone.c4ui
 
 import ee.cone.c4vdom.Types.ViewRes
-import ee.cone.c4vdom.{ToChildPair, ToJson, c4el, c4tagSwitch, c4tags, c4val}
-
-@c4tagSwitch("FrontApp") trait ColorDef extends ToJson
+import ee.cone.c4vdom.{Receiver, ToChildPair, c4el, c4tags}
 
 trait InlineElement extends ToChildPair
 
-@c4tags("FrontApp") trait InlineElementsTags {
-  @c4val("p") def paletteColor(
-    cssClass: String,
-  ): ColorDef
-
-  @c4val("r") def rawColor(
-    bgColor: String,
-    textColor: String,
-  ): ColorDef
+@c4tags("FrontApp") trait InlineElementsTags[C] {
 
   @c4el("InlineButton") def inlineButton(
     key: String,
     color: ColorDef,
     children: ViewRes,
+    receiver: Receiver[C] = NoReceiver[C],
   ): InlineElement
 
   @c4el("InlineChip") def inlineChip(
     key: String,
     color: ColorDef,
     children: ViewRes,
+    receiver: Receiver[C] = NoReceiver[C],
   ): InlineElement
 }
