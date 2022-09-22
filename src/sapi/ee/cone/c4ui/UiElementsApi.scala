3bfc1450238ea601e17b1aeab6e3465a4e85ff12
Dmitry Nor <dn@cone.ee>
2022-09-22 17:47:13 +0300

    Removed Flexible

---
+++ src/sapi/ee/cone/c4ui/UiElementsApi.scala
@@ -0,0 +1,94 @@
+package ee.cone.c4ui
+
+import ee.cone.c4di._
+import ee.cone.c4ui.FrontTypes.Em
+import ee.cone.c4vdom.Types._
+import ee.cone.c4vdom._
+
+@c4tagSwitch("FrontApp") trait Size extends ToJson {
+  def min: Em
+  def max: Option[Em]
+}
+
+@c4tagSwitch("FrontApp") trait Align extends ToJson
+
+@c4tagSwitch("FrontApp") trait GroupboxDisplay extends ToJson
+
+@c4tagSwitch("FrontApp") trait ChildAlign extends ToJson
+
+trait UIElement extends ToChildPair
+
+@c4tags("FrontApp") trait UIElements {
+  @c4val def size(
+    min: Em,
+    max: Option[Em],
+  ): Size
+
+  @c4val("l") def left: Align
+  @c4val("c") def center: Align
+  @c4val("r") def right: Align
+  @c4val("f") def fill: Align
+
+  @c4el("FlexibleColumnRoot") def columnRoot(
+    key: String,
+    children: ElList[UIElement]
+  ): ToChildPair
+
+  @c4el("FlexibleColumn") def column(
+    key: String,
+    sizes: Option[Size],
+    align: Align,
+    children: ViewRes,
+    className: Option[String] = None,
+  ): UIElement
+
+  @c4val("accent") def groupboxAccented: GroupboxDisplay
+
+  @c4el("FlexibleGroupbox") def groupbox(
+    key: String,
+    sizes: Option[Size],
+    align: Align,
+    children: ElList[UIElement],
+    label: Option[String] = None,
+    displayStyle: Option[GroupboxDisplay] = None
+  ): UIElement
+
+  @c4el("FlexibleRow") def row(
+    key: String,
+    sizes: Option[Size],
+    align: Align,
+    children: ViewRes,
+    className: Option[String] = None,
+  ): UIElement
+
+  @c4el("ThinFlexibleRow") def thinRow(
+    key: String,
+    sizes: Option[Size],
+    align: Align,
+    children: ViewRes,
+    className: Option[String] = None,
+  ): UIElement
+
+  @c4val("FlexibleChildAlign") def childAlign(
+    align: Option[Align],
+  ): ChildAlign
+
+  @c4el("FlexibleCell") def cell(
+    key: String,
+    sizes: Option[Size],
+    grow: Option[Boolean],
+    align: Align,
+    children: ViewRes,
+    className: Option[String] = None,
+  ): UIElement
+
+  @c4el("FlexibleLabeled") def labeled(
+    key: String,
+    sizes: Option[Size],
+    align: Align,
+    label: String,
+    children: ViewRes,
+    labelChildren: ViewRes = Nil,
+    horizontal: Boolean = false,
+  ): UIElement
+}
