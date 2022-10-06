c3857f7e5699c718a7aced0311e90ebd8d167930
Dmitry Nor <dn@cone.ee>
2022-10-06 19:08:30 +0300

    VirtualKeyboardElements API (no BigDecimal)

---
+++ src/sapi/ee/cone/c4ui/VirtualKeyboardApi.scala
@@ -0,0 +1,48 @@
+package ee.cone.c4ui
+
+import ee.cone.c4ui.FrontTypes.Em
+import ee.cone.c4vdom.{ToChildPair, ToJson, c4el, c4tagSwitch, c4tags, c4val}
+
+@c4tagSwitch("FrontApp") trait KeyboardPosition extends ToJson
+
+@c4tagSwitch("FrontApp") trait KeyboardType extends ToJson
+
+@c4tagSwitch("FrontApp") trait KeyboardMode extends ToJson
+
+@c4tagSwitch("FrontApp") trait Key extends ToJson
+
+
+@c4tags("FrontApp") trait VirtualKeyboardElements {
+
+  @c4val("left") def left: KeyboardPosition
+  @c4val("right") def right: KeyboardPosition
+  @c4val("bottom") def bottom: KeyboardPosition
+  @c4val("static") def static: KeyboardPosition
+
+  @c4val def key(
+    key: String,
+    /*row: BigDecimal,
+    column: BigDecimal,
+    width: Option[BigDecimal] = Some(1),
+    height: Option[BigDecimal] = Some(1),*/
+    symbol: String = "",
+    color: ColorDef,
+  ): Key
+
+  @c4val def keyboardMode(
+    keys: List[Key],
+  ): KeyboardMode
+
+  @c4val def keyboardType(
+    name: String,
+    modes: List[KeyboardMode],
+  ): KeyboardType
+
+  @c4el("VirtualKeyboard") def virtualKeyboard(
+    key: String, //VDomKey
+    keyboardTypes: List[KeyboardType],
+    position: KeyboardPosition,
+    scale: Em,
+  ): ToChildPair
+}
+
