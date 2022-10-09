5714758f0826639cfd4e1817463d616ce66d9c0b
Aleafan <ae.afanasov@yandex.com>
2022-10-10 00:36:24 +0200

    add max-width & height calculation

--- src/extra/virtual-keyboard.tsx
+++ src/extra/virtual-keyboard.tsx
@@ -33,26 +33,33 @@ interface Key {
 }
 
  function VirtualKeyboard({ keyboardTypes }: VirtualKeyboard) {
-    // @ts-ignore
-    const { rowsTotal, colsTotal } = keyboardTypes[0].modes[0];
+    // const { rowsTotal, colsTotal } = keyboardTypes[0].modes[0];
+    const [ rowsTotal, colsTotal ] = keyboardTypes[0].modes[0].keys.reduce((dimensions, { row, column, width, height }) => {
+        const colMax = column + width - 1;
+        const rowsTotal =  colMax > dimensions[0] ? colMax : dimensions[0]; // TODO: function!
+        const rowMax = row + height - 1;
+        const colsTotal = rowMax > dimensions[1] ? rowMax : dimensions[1];
+        return [rowsTotal, colsTotal];
+    }, [0, 0]);
+    
     const wrapperStyle: CSSProperties = {
         height: `${VK_ROW_HEIGHT * rowsTotal}em`,
         maxWidth: `${VK_COL_WIDTH * colsTotal}em`,
     }
     return (
-        <div style={wrapperStyle} className='virtualKeyboard' >
-            {keyboardTypes[0].modes[0].keys.map((btn, ind) => {
-                const { key, symbol, row, column, width, height } = btn;
-                const btnStyle: CSSProperties = {
-                    position: 'absolute',
-                    left: `${(column - 1) * 100 / colsTotal}%`,
-                    top: `${VK_ROW_HEIGHT * (row - 1)}em`,
-                    width: `${width * 100 / colsTotal}%`,
-                    height: `${VK_ROW_HEIGHT * height}em`,
-                }
-                return <VKKey key={`${key}-${ind}`} style={btnStyle} {...{ keyCode: key, symbol }} />
-            })}
-        </div>
+            <div style={wrapperStyle} className='virtualKeyboard' >
+                {keyboardTypes[0].modes[0].keys.map((btn, ind) => {
+                    const { key, symbol, row, column, width, height } = btn;
+                    const btnStyle: CSSProperties = {
+                        position: 'absolute',
+                        left: `${(column - 1) * 100 / colsTotal}%`,
+                        top: `${VK_ROW_HEIGHT * (row - 1)}em`,
+                        width: `${width * 100 / colsTotal}%`,
+                        height: `${VK_ROW_HEIGHT * height}em`,
+                    }
+                    return <VKKey key={`${key}-${ind}`} style={btnStyle} {...{ keyCode: key, symbol }} />
+                })}
+            </div>
     );
  }
 
