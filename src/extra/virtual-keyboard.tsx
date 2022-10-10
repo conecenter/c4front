168994902b92888f3fec14bea22fb45438bf908c
Aleafan <ae.afanasov@yandex.com>
2022-10-10 14:16:46 +0200

    * solve scroll problem *add input + type logic *add show logic + setup

--- src/extra/virtual-keyboard.tsx
+++ src/extra/virtual-keyboard.tsx
@@ -1,6 +1,11 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, MutableRefObject, useContext, useRef, useState } from 'react';
+import { PathContext } from './focus-control';
 import { ColorDef } from './view-builder/common-api';
+import { createElement as el, ReactNode, useCallback, useMemo} from 'react'
+import {ScrollInfoContext, ScrollInfo} from "../extra/scroll-info-context";
+import {useAnimationFrame} from "../main/vdom-hooks";
 
+const BOTTOM_ROW_CLASS = "bottom-row";
 const VK_COL_WIDTH = 2;
 const VK_ROW_HEIGHT = 2.2;
 
@@ -8,7 +13,8 @@ interface VirtualKeyboard {
     key: string,
     identity: Object,
     keyboardTypes: KeyboardType[],
-    position: "left" | "right" | "bottom" | "static"
+    position: "left" | "right" | "bottom" | "static",
+    setupType?: string
 }
 
 interface KeyboardType {
@@ -32,23 +38,40 @@ interface Key {
     color?: ColorDef
 }
 
- function VirtualKeyboard({ keyboardTypes }: VirtualKeyboard) {
+function VirtualKeyboard({ keyboardTypes, setupType }: VirtualKeyboard) {
+    const vkRef = useRef<HTMLDivElement | null>(null);
+    
+    // Show VK logic
+    const [showVk, setShowVk] = useState(false);
+    const currentPath = useContext(PathContext);
+    const inputInFocus = getInputInFocus(vkRef, currentPath);
+    if (setupType || inputInFocus) !showVk && setShowVk(true);
+    else showVk && setShowVk(false);
+    if (!showVk) return null;
+
+    // Get keyboard type
+    const keyboardTypeName = setupType || inputInFocus?.dataset?.type || 'text';
+    const keyboardType = keyboardTypes.find(type => type.name === keyboardTypeName);
+    if (!keyboardType) return null;
+
+    // Positioning logic
     // const { rowsTotal, colsTotal } = keyboardTypes[0].modes[0];
-    const [ rowsTotal, colsTotal ] = keyboardTypes[0].modes[0].keys.reduce((dimensions, { row, column, width, height }) => {
+    const [ rowsTotal, colsTotal ] = keyboardType.modes[0].keys.reduce((dimensions, key) => {
+        const { row, column, width, height } = key;
         const colMax = column + width - 1;
         const rowsTotal =  colMax > dimensions[0] ? colMax : dimensions[0]; // TODO: function!
         const rowMax = row + height - 1;
         const colsTotal = rowMax > dimensions[1] ? rowMax : dimensions[1];
         return [rowsTotal, colsTotal];
     }, [0, 0]);
-    
+
     const wrapperStyle: CSSProperties = {
         height: `${VK_ROW_HEIGHT * rowsTotal}em`,
         maxWidth: `${VK_COL_WIDTH * colsTotal}em`,
     }
     return (
-            <div style={wrapperStyle} className='virtualKeyboard' >
-                {keyboardTypes[0].modes[0].keys.map((btn, ind) => {
+            <div ref={vkRef}  style={wrapperStyle} className={`virtualKeyboard ${BOTTOM_ROW_CLASS}`} >
+                {keyboardType.modes[0].keys.map((btn, ind) => {
                     const { key, symbol, row, column, width, height } = btn;
                     const btnStyle: CSSProperties = {
                         position: 'absolute',
@@ -60,8 +83,14 @@ interface Key {
                     return <VKKey key={`${key}-${ind}`} style={btnStyle} {...{ keyCode: key, symbol }} />
                 })}
             </div>
-    );
- }
+        );
+}
+
+ function getInputInFocus(domRef: MutableRefObject<HTMLDivElement | null>, currentPath: string) {
+    const cNode = domRef.current?.ownerDocument.querySelector(`[data-path]=${currentPath}`);
+    const input = cNode?.querySelector<HTMLInputElement>('input:not([readonly])');
+    return input;
+}
 
 
  interface VKKey {
@@ -80,3 +109,304 @@ interface Key {
  }
 
  export { VirtualKeyboard };
+
+
+
+
+interface ScrollInfoState extends ScrollInfo {
+  currentScrollOffset: number,
+  scrollingUp: boolean,
+  scrollUpStart: number,
+  scrollDownStart: number,
+  totalTopHeight: number,
+  totalBottomHeight: number,
+  totalFilterHeight: number,
+  classes: string
+}
+
+const defaultValue: ScrollInfoState = {
+  currentScrollOffset: 0,
+  scrollingUp: false,
+  scrollUpStart: 0,
+  scrollDownStart: 0,
+  elementsStyles: new Map(),
+  totalTopHeight: 0,
+  totalBottomHeight: 0,
+  totalFilterHeight: 0,
+  classes: ""
+}
+
+interface ScrollInfoProviderProps {
+  children: ReactNode[]
+}
+
+const HIDE_ON_SCROLL = "hideOnScroll"
+const TOP_ROW_CLASS = "topRow"
+const DATA_PATH = "data-path"
+const TOP_ROW_SELECTOR = ".topRow"
+const BOTTOM_ROW_SELECTOR = ".bottom-row"
+const GRID_WRAP_SELECTOR = ".gridWrap"
+const FILTER_AREA_SELECTOR = ".filterArea"
+
+const MIN_SCROLL_DELTA = 5
+
+interface ScrollDiff {
+  scrollingUp: boolean,
+  scrollUpStart: number,
+  scrollDownStart: number,
+}
+
+function calculateScrollDiff(
+  {
+    currentScrollOffset: previousScroll,
+    scrollUpStart,
+    scrollDownStart,
+    scrollingUp,
+  }: ScrollInfoState,
+  currentScrollOffset: number,
+  totalTopHeight: number,
+  totalBottomHeight: number,
+): ScrollDiff {
+  if (currentScrollOffset == 0) // Initial position
+    return {scrollingUp: false, scrollUpStart: 0, scrollDownStart: 0}
+  else if (scrollingUp && currentScrollOffset > previousScroll + MIN_SCROLL_DELTA) // Scrolling down after scrolling up
+    return {scrollingUp: false, scrollUpStart: scrollUpStart, scrollDownStart: currentScrollOffset + totalBottomHeight}
+  else if (!scrollingUp && currentScrollOffset < previousScroll - MIN_SCROLL_DELTA) // Scrolling up after scrolling down
+    return {scrollingUp: true, scrollUpStart: Math.max(currentScrollOffset - totalTopHeight, 0), scrollDownStart: currentScrollOffset}
+  else
+    return {scrollingUp: scrollingUp, scrollUpStart: scrollUpStart, scrollDownStart: scrollDownStart}
+}
+
+interface TopScrollPositions {
+  spaceUsed: number,
+  alwaysShownHeight: number,
+  floatingPos: number,
+  topElementsStyles: Map<string, string>
+}
+
+function calculateTopPositions(
+  {scrollUpStart}: ScrollDiff,
+  currentScrollOffset: number,
+  topElements: HTMLElement[] | null,
+): TopScrollPositions {
+  const initialState: TopScrollPositions = {
+    spaceUsed: 0,
+    alwaysShownHeight: 0,
+    floatingPos: Math.min(scrollUpStart - currentScrollOffset, 0),
+    topElementsStyles: new Map()
+  }
+
+  function iteration(
+    {spaceUsed, alwaysShownHeight, floatingPos, topElementsStyles}: TopScrollPositions,
+    elem: HTMLElement
+  ): TopScrollPositions {
+    const alwaysShown = !elem.classList.contains(HIDE_ON_SCROLL)
+    const elemTop = alwaysShown ? Math.max(alwaysShownHeight, floatingPos) : floatingPos
+    const elemHeight = elem.offsetHeight
+    return {
+      spaceUsed: Math.max(elemTop + elemHeight, spaceUsed),
+      alwaysShownHeight: alwaysShown ? alwaysShownHeight + elemHeight : alwaysShownHeight,
+      floatingPos: floatingPos + elemHeight,
+      topElementsStyles: topElementsStyles.set(elem.getAttribute(DATA_PATH) + "", elemTop + 'px')
+    }
+  }
+
+  const result = topElements ? topElements.reduce(iteration, initialState) : initialState
+  return {...result, spaceUsed: Math.max(result.spaceUsed, 0)}
+}
+
+function calculateBottomPositions(
+  {scrollDownStart}: ScrollDiff,
+  currentScrollOffset: number,
+  bottomElements: HTMLElement[] | null,
+): Map<string, string> {
+  const initialState: TopScrollPositions = {
+    spaceUsed: 0,
+    alwaysShownHeight: 0,
+    floatingPos: Math.min(currentScrollOffset - scrollDownStart, 0),
+    topElementsStyles: new Map()
+  }
+
+  function iteration(
+    {spaceUsed, alwaysShownHeight, floatingPos, topElementsStyles}: TopScrollPositions,
+    elem: HTMLElement
+  ): TopScrollPositions {
+    const alwaysShown = !elem.classList.contains(HIDE_ON_SCROLL)
+    const elemTop = alwaysShown ? Math.max(alwaysShownHeight, floatingPos) : floatingPos
+    const elemHeight = elem.offsetHeight
+    return {
+      spaceUsed: Math.max(elemTop + elemHeight, spaceUsed),
+      alwaysShownHeight: alwaysShown ? alwaysShownHeight + elemHeight : alwaysShownHeight,
+      floatingPos: floatingPos + elemHeight,
+      topElementsStyles: topElementsStyles.set(elem.getAttribute(DATA_PATH) + "", elemTop + 'px')
+    }
+  }
+
+  const result = bottomElements ? bottomElements.reverse().reduce(iteration, initialState) : initialState
+  return result.topElementsStyles
+}
+
+interface FilterAreaPosition {
+  filterSpaceUsed: number,
+  floatingPos: number
+}
+
+function calculateFilterArea(
+  filterAreas: HTMLElement[],
+  spaceUsed: number,
+  floatingPos: number
+): FilterAreaPosition {
+  const initialState: FilterAreaPosition = {
+    filterSpaceUsed: spaceUsed,
+    floatingPos: floatingPos,
+  }
+
+  function iteration(
+    {filterSpaceUsed, floatingPos}: FilterAreaPosition,
+    elem: HTMLElement
+  ): FilterAreaPosition {
+    const elemTop = floatingPos
+    const elemHeight = elem.offsetHeight
+    return {
+      filterSpaceUsed: Math.max(elemTop + elemHeight, filterSpaceUsed),
+      floatingPos: floatingPos + elemHeight,
+    }
+  }
+
+  return filterAreas.reduce(iteration, initialState)
+}
+
+function calculateGridClasses(
+  ownerDocument: Document | null,
+  topSpaceUsed: number,
+  floatingPos: number
+): string {
+  const gridWrappers = ownerDocument ? [...ownerDocument.querySelectorAll<HTMLElement>(GRID_WRAP_SELECTOR)] : []
+
+  const classes = gridWrappers.map(gridWrapper => {
+    const {filterSpaceUsed} = calculateFilterArea([...gridWrapper.querySelectorAll<HTMLElement>(".filterArea")], topSpaceUsed, floatingPos)
+    const grid = gridWrapper.querySelectorAll(".grid")[0]
+    const gridKey = grid.getAttribute("data-grid-key") + ""
+    const headerRows = (grid.getAttribute("header-row-keys") + "").split(" ")
+    let rowHeightMap = new Map<string, number>()
+    const headers = [...grid.querySelectorAll(".tableHeadContainer")]
+    headers.forEach(header => {
+      const rowKey = header.getAttribute("data-row-key") + ""
+      if (!rowHeightMap.has(rowKey)) {
+        rowHeightMap.set(rowKey, header.getBoundingClientRect().height)
+      }
+    })
+    let headerRowToOffset = new Map<string, number>()
+
+    function reduceHeight(current: number, rowKey: string): number {
+      headerRowToOffset.set(rowKey, current)
+      // @ts-ignore
+      return current + (rowHeightMap.get(rowKey) ? rowHeightMap.get(rowKey) : 0)
+    }
+
+    headerRows.reduce(reduceHeight, filterSpaceUsed)
+    return [...headerRowToOffset.entries()].map(elem => `.grid[data-grid-key="${gridKey}"] > .tableHeadContainer[data-row-key="${elem[0]}"]{z-index: 450;position:sticky;top:${elem[1]}px}`)
+  })
+  return classes.map(classList => classList.join("\n")).join("\n")
+
+}
+
+interface GetElements {
+  elements: HTMLElement[] | null,
+  elementsLength: number,
+  elementsHeight: number
+}
+
+function getElements(ownerDocument: Document | null, selector: string): GetElements {
+  const elements: HTMLElement[] | null = ownerDocument ? [...ownerDocument.querySelectorAll<HTMLElement>(selector)] : []
+  const elementsLength = elements ? elements.length : 0
+  const elementsHeight = elements.reduce(((prev, currentElement) => prev + currentElement.offsetHeight), 0)
+  return {elements: elements, elementsLength: elementsLength, elementsHeight: elementsHeight}
+}
+
+function ScrollInfoProvider({children}: ScrollInfoProviderProps) {
+  const [scrollInfo, setScrollInfo] = useState(defaultValue)
+  const [docRoot, setDocRoot] = useState<Element | null>(null)
+  const ownerDocument = docRoot ? docRoot.ownerDocument : null
+  const ownerWindow = ownerDocument && ownerDocument.defaultView ? ownerDocument.defaultView : null
+
+  const listener = useCallback(e => {
+    setScrollInfo((prev: ScrollInfoState) => {
+      const currentScrollOffset = ownerWindow ? ownerWindow.pageYOffset : 0
+
+      const {elements: topElements, elementsLength: topElementsLength, elementsHeight: totalTopHeight} = getElements(ownerDocument, TOP_ROW_SELECTOR)
+      const {elements: bottomElements, elementsLength: bottomElementsLength, elementsHeight: totalBottomHeight} = getElements(ownerDocument, BOTTOM_ROW_SELECTOR)
+      const {elementsHeight: totalFilterHeight} = getElements(ownerDocument, FILTER_AREA_SELECTOR)
+
+      if (prev.currentScrollOffset === currentScrollOffset &&
+        topElementsLength + bottomElementsLength == prev.elementsStyles.size &&
+        totalTopHeight == prev.totalTopHeight && 
+        totalBottomHeight == prev.totalBottomHeight &&
+        totalFilterHeight == prev.totalFilterHeight
+      ) return prev;
+      else {
+        const scrollDiff = calculateScrollDiff(prev, currentScrollOffset, totalTopHeight, totalBottomHeight)
+        // top-rows
+        const {
+          spaceUsed: topSpaceUsed,
+          topElementsStyles,
+          floatingPos
+        } = calculateTopPositions(scrollDiff, currentScrollOffset, topElements)
+        // bottom-rows
+        const bottomElementStyles = calculateBottomPositions(scrollDiff, currentScrollOffset, bottomElements)
+        // grid
+        const gridClasses = calculateGridClasses(ownerDocument, topSpaceUsed, floatingPos)
+        const filterClasses = gridClasses !== "" ? `\n.filterArea{z-index: 500;position:sticky !important;top:${floatingPos}px}` : ""
+        return {
+          currentScrollOffset: currentScrollOffset,
+          scrollingUp: scrollDiff.scrollingUp,
+          scrollUpStart: scrollDiff.scrollUpStart,
+          scrollDownStart: scrollDiff.scrollDownStart,
+          elementsStyles: new Map([...topElementsStyles, ...bottomElementStyles]),
+          totalTopHeight: totalTopHeight,
+          totalBottomHeight: totalBottomHeight,
+          totalFilterHeight: totalFilterHeight,
+          classes: gridClasses + filterClasses
+        }
+      }
+    })
+  }, [docRoot]);
+  const {classes, totalTopHeight, totalBottomHeight} = useMemo(
+    () => (
+      {
+        classes: scrollInfo.classes,
+        totalTopHeight: scrollInfo.totalTopHeight,
+        totalBottomHeight: scrollInfo.totalBottomHeight,
+      }),
+    [
+      scrollInfo.classes,
+      scrollInfo.totalTopHeight,
+      scrollInfo.totalBottomHeight
+    ]
+  )
+  const scrollInfoCached = useMemo<ScrollInfo>(
+    () => (
+      {
+        elementsStyles: scrollInfo.elementsStyles,
+      }),
+    [
+      scrollInfo.elementsStyles
+    ]
+  )
+
+  useAnimationFrame(docRoot, listener)
+
+  return el(
+    "div",
+    {
+      key: "scroll-info-provider",
+      style: {paddingTop: totalTopHeight + "px", paddingBottom: totalBottomHeight + "px"},
+      ref: setDocRoot
+    },
+    el("style", {dangerouslySetInnerHTML: {__html: classes}}),
+    el(ScrollInfoContext.Provider, {value: scrollInfoCached}, children)
+  )
+}
+
+export {ScrollInfoProvider, HIDE_ON_SCROLL, BOTTOM_ROW_CLASS, TOP_ROW_CLASS, DATA_PATH};
+export type {ScrollInfo};
--- src/test/virtual-keyboard/vdom-vk-app.ts
+++ src/test/virtual-keyboard/vdom-vk-app.ts
@@ -1,41 +1,47 @@
 import ReactDOM from "react-dom"
 import React, { createElement as $ } from "react"
 import { createSyncProviders } from "../../main/vdom-hooks";
-import { VirtualKeyboard } from "../../extra/virtual-keyboard";
+import { ScrollInfoProvider, VirtualKeyboard } from "../../extra/virtual-keyboard";
 
 
 function App() {
-    const child = $(VirtualKeyboard, {
-        key: "TEST1",
-        identity: {parent: "test"},
-        position: 'bottom',
-        keyboardTypes: [
-            {
-                name: 'number',
-                modes: [
+    const child = $(ScrollInfoProvider, null,
+        $('div', {style: {fontSize: '5em'}},
+            "jgjsdlgj dsgdslgjldfjg fjgfg  gadjfg fdgf gjf gfdgfdj gfdgfdjga ajglkfjgjlf gfag fgfadljglajlgjlajg glajgl lf gjfjg fdgfdl gagjafjgj agj fald glfadjlgj adfglfd glfa jglf dglfdl gfdlg fdjgfdjlf gfdl g",
+            $(VirtualKeyboard, {
+                key: "TEST1",
+                identity: {parent: "test"},
+                position: 'bottom',
+                setupType: 'text',
+                keyboardTypes: [
                     {
-                        keys: [
-                            { key: '7', row: 1, column: 1, width: 1, height: 1 },
-                            { key: '8', row: 1, column: 2, width: 1, height: 1 },
-                            { key: '9', row: 1, column: 3, width: 1, height: 1 },
-                            { key: 'Backspace', symbol: '⌫', row: 1, column: 4, width: 1, height: 1 },
-                            { key: '4', row: 2, column: 1, width: 1, height: 1 },
-                            { key: '5', row: 2, column: 2, width: 1, height: 1 },
-                            { key: '6', row: 2, column: 3, width: 1, height: 1 },
-                            { key: 'C', row: 2, column: 4, width: 1, height: 1 },
-                            { key: '1', row: 3, column: 1, width: 1, height: 1 },
-                            { key: '2', row: 3, column: 2, width: 1, height: 1 },
-                            { key: '3', row: 3, column: 3, width: 1, height: 1 },
-                            { key: '0', row: 4, column: 1, width: 3, height: 1 },
-                            { key: 'Enter', symbol: '⏎', row: 3, column: 4, width: 1, height: 2 },
-                        ],
-                        rowsTotal: 4,
-                        colsTotal: 4
+                        name: 'text',
+                        modes: [
+                            {
+                                keys: [
+                                    { key: '7', row: 1, column: 1, width: 1, height: 1 },
+                                    { key: '8', row: 1, column: 2, width: 1, height: 1 },
+                                    { key: '9', row: 1, column: 3, width: 1, height: 1 },
+                                    { key: 'Backspace', symbol: '⌫', row: 1, column: 4, width: 1, height: 1 },
+                                    { key: '4', row: 2, column: 1, width: 1, height: 1 },
+                                    { key: '5', row: 2, column: 2, width: 1, height: 1 },
+                                    { key: '6', row: 2, column: 3, width: 1, height: 1 },
+                                    { key: 'C', row: 2, column: 4, width: 1, height: 1 },
+                                    { key: '1', row: 3, column: 1, width: 1, height: 1 },
+                                    { key: '2', row: 3, column: 2, width: 1, height: 1 },
+                                    { key: '3', row: 3, column: 3, width: 1, height: 1 },
+                                    { key: '0', row: 4, column: 1, width: 3, height: 1 },
+                                    { key: 'Enter', symbol: '⏎', row: 3, column: 4, width: 1, height: 2 },
+                                ],
+                                rowsTotal: 4,
+                                colsTotal: 4
+                            }
+                        ]
                     }
                 ]
-            }
-        ]
-    })
+            })
+        )
+    )
     const sender = {
         enqueue: (identity: any, patch: any) => console.log(patch)
     }
