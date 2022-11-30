21700b72f4db3229d7a97ef843bdfe2009781aa1
Aleafan <aaf@cone.ee>
2022-11-30 13:40:57 +0100

    refactor RouteElement to accept chips

--- src/extra/route-element.tsx
+++ src/extra/route-element.tsx
@@ -1,7 +1,5 @@
-import React, { ReactNode, useMemo, useRef } from 'react';
+import React, { ReactElement, useMemo, useRef } from 'react';
 import clsx from 'clsx';
-import { getPath, useFocusControl } from './focus-control';
-import { usePatchSync } from './exchange/patch-sync';
 import { useAddEventListener } from './custom-hooks';
 import { COPY_EVENT } from './focus-module-interface';
 
@@ -10,31 +8,20 @@ interface RouteElementProps {
     key: string,
     identity: Object,
     compact?: boolean,
-    routeParts: RoutePartData[]
- }
- 
- interface RoutePartData {
-    text: string,
-    done: boolean,
-    hint?: string,
-    children?: ReactNode[]
+    routeParts: ReactElement[]  // ChipElements
 }
 
 function RouteElement({identity, compact, routeParts}: RouteElementProps) {
-    const lastDone = routeParts.findIndex(part => !part.done) - 1;
-    const path = useMemo(() => getPath(identity), [identity]);
-
-    const { handleClick } = useRouteElementSync(identity, 'receiver');
+    const routeElemRef = useRef(null);
 
     // Copy on Ctrl+C functionality
-    const routeElemRef = useRef(null);
 	useAddEventListener(routeElemRef.current, COPY_EVENT, handleClipboardWrite);
 
     async function handleClipboardWrite(e: CustomEvent) {
 		// On Firefox writing to the clipboard is blocked for non user-initiated event callbacks
         e.stopPropagation();
 		try {
-            const wholeCode = routeParts.reduce((accum, part) => accum + part.text, '');
+            const wholeCode = routeParts.reduce((accum, elem) => accum + (elem.props?.value ?? ''), '');
 			await navigator.clipboard.writeText(wholeCode);
 		} catch(err) {
 			console.log(err);
@@ -43,45 +30,9 @@ function RouteElement({identity, compact, routeParts}: RouteElementProps) {
 
     return (
         <div ref={routeElemRef} className={clsx('routeElement', compact && 'compact')}>
-            {routeParts.map((part, ind) => {
-                const { text, hint, done } = part;
-                const isLastDone = ind === lastDone;
-
-                const partPath = `${path}/part-${ind}`;
-                const { focusClass, focusHtml } = useFocusControl(partPath);
-                return (
-                    <div key={`${ind}`}
-                         {...focusHtml}
-                         className={clsx(done && 'routePartDone', isLastDone && 'isLastDone', focusClass)}
-                         // style={onClick ? {cursor: 'pointer'} : undefined}
-                         title={hint}
-                         onClick={() => handleClick(ind)} >
-                        {text}
-                    </div>
-                );
-            })}
+            {routeParts}
         </div>
     );
 }
-
-function useRouteElementSync(
-    identity: Object,
-    receiverName: string
-) {
-    const {sendFinalChange} = usePatchSync<string, string, string>(
-        identity,
-        receiverName,
-        '',
-        false,
-        (b) => b,
-        (b) => ({
-            headers: {"x-r-clicked-part": b},
-            value: ""
-        }),
-        (p) => '',
-        (prevState, ch) => ch
-    );
-    return { handleClick: (index: number) => sendFinalChange(String(index)) }
-  }
 
  export { RouteElement };
