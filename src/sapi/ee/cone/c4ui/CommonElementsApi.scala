2028fd3568bf45fcaa6722188e7e27fedfca4ca6
Dmitry Nor <dn@cone.ee>
2023-01-08 23:09:18 +0200

    new Joiners View, checkpoint CU-85zrg845a

--- src/sapi/ee/cone/c4ui/CommonElementsApi.scala
+++ src/sapi/ee/cone/c4ui/CommonElementsApi.scala
@@ -1,8 +1,12 @@
 package ee.cone.c4ui
 
-import ee.cone.c4vdom.{ToJson, c4tagSwitch, c4tags, c4val}
+import ee.cone.c4vdom.{ToChildPair, ToJson, c4el, c4tagSwitch, c4tags, c4val}
 
 @c4tagSwitch("FrontApp") trait ColorDef extends ToJson
+
+@c4tagSwitch("FrontApp") trait Row extends ToJson
+
+@c4tagSwitch("FrontApp") trait Text extends ToJson
 
 @c4tags("FrontApp") trait CommonElementsTags {
   @c4val("p") def paletteColor(
@@ -14,4 +18,18 @@ import ee.cone.c4vdom.{ToJson, c4tagSwitch, c4tags, c4val}
     textColor: String,
   ): ColorDef
 
+  @c4val("Text") def textElement(
+    text: String,
+    color: Option[ColorDef] = None,
+  ): Text
+
+  @c4val("Row") def rowElement(
+    text: List[Text],
+  ): Row
+
+  @c4el("RichTextElement") def richTextElement(
+    key: String,
+    text: List[Row],
+    color: Option[ColorDef] = None
+  ): ToChildPair
 }
