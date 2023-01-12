aa596093043e387a97738a4c4d99398a608564c6
Aleafan <aaf@cone.ee>
2023-01-12 17:05:43 +0100

    move user manual code to front (CU-85zrghpuq)

---
+++ src/extra/user-manual.ts
@@ -0,0 +1,77 @@
+import {createContext, createElement as $, ReactNode, useContext, useEffect, useState, useMemo} from "react"
+import type {KeyboardEvent, SyntheticEvent} from "react"
+
+interface UserManual {
+    has: (umid: string | undefined) => boolean
+    getUrl: (umid: string | undefined) => string | null
+}
+
+const defaultUserManual: UserManual = {
+    has: umid => false,
+    getUrl: umid => null
+}
+
+const UserManualContext = createContext<UserManual>(defaultUserManual)
+UserManualContext.displayName = "UserManualContext"
+
+const useUserManual: () => UserManual = () => useContext(UserManualContext)
+
+interface UserManualProviderProps {
+    url: string,
+    children: ReactNode
+}
+
+function UserManualProvider({url, children}: UserManualProviderProps) {
+    const [userManualText, setUserManualText] = useState<string>("{}")
+    useEffect(() => {
+        fetch(url)
+            .then(resp => resp.text())
+            .then(setUserManualText)
+            .catch((reason) => {
+                console.log(reason)
+                setUserManualText("{}")
+            })
+    }, [url])
+    const providerValue = useMemo(() => {
+        const userManual = JSON.parse(userManualText)
+        const has = (umid: string | undefined) => umid && umid in userManual ? true : false
+        const getUrl = (umid: string | undefined) => umid && umid in userManual ? userManual[umid] : null
+        return {has, getUrl}
+    }, [userManualText])
+
+    return $(UserManualContext.Provider, {value: providerValue, children})
+}
+
+function getUserManualUtils(url: string | null): { button?: ReactNode, onKeyDown?: ((e: KeyboardEvent) => void) } {
+    const action = (e: SyntheticEvent) => {
+        if (url) {
+            e.stopPropagation()
+            e.preventDefault()
+            window.open(url)
+        }
+    }
+    const keyDown = (e: KeyboardEvent) => {
+        if (e.code == "F1") {
+            action(e)
+        }
+    }
+    return url ? {button: UserManualButton(action), onKeyDown: keyDown} : {}
+}
+
+function UserManualButton(action: (e: SyntheticEvent) => void) {
+    return $("div", {
+            onClick: action,
+            style: {
+                cursor: "pointer",
+                width: "0.8em",
+                position: "absolute",
+                right: "0em",
+                top: "-1em",
+                zIndex: "150000"
+            }
+        },
+        $("img", { src: '/mod/main/ee/cone/core/ui/c4view/info.svg' })
+    )
+}
+
+export {UserManualProvider, useUserManual, getUserManualUtils}
