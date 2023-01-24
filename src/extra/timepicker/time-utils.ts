37b55683664a2d2f0588c92dcdc0c6bb747065db
Aleafan <aaf@cone.ee>
2023-01-24 18:59:26 +0100

    finish arrows control logic

--- src/extra/timepicker/time-utils.ts
+++ src/extra/timepicker/time-utils.ts
@@ -1,5 +1,44 @@
 import { NumberToken, TimeToken, Token, tokenizeString } from "../datepicker/date-utils";
 import { InputState, TimestampState } from "./timepicker";
+import { milliseconds } from "date-fns";
+
+// Time calculations helpers
+const MAX_TIMESTAMP = milliseconds({hours: 24});
+
+const TIME_TOKENS = ['H', 'm', 's', 'S'];
+
+const getHours = (ms: number) => Math.floor(ms / TOKEN_DATA.H.ms) % 24;
+const getMinutes = (ms: number) => Math.floor(ms / TOKEN_DATA.m.ms) % 60;
+const getSeconds = (ms: number) => Math.floor(ms / TOKEN_DATA.s.ms) % 60;
+const getMilliseconds = (ms: number) => ms % TOKEN_DATA.s.ms; 
+
+interface TokenInfo {
+    [index: string]: { ms: number, max: number, get: (ms: number) => number }
+}
+
+const TOKEN_DATA: TokenInfo = {
+    H: {
+        ms: milliseconds({hours: 1}), 
+        max: 24,
+        get: getHours
+    },
+    m: {
+        ms: milliseconds({minutes: 1}),
+        max: 60,
+        get: getMinutes
+    },
+    s: {
+        ms: 1000,
+        max: 60,
+        get: getSeconds
+    },
+    S: {
+        ms: 1,
+        max: 1000,
+        get: getMilliseconds
+    }
+}
+
 
 // Timepicker state helpers
 const isInputState = (state: TimestampState | InputState): state is InputState => state.tp === 'input-state';
@@ -12,38 +51,29 @@ const createInputChange = (inputValue: string, tempTimestamp?: number): InputSta
 
 const createTimestampChange = (timestamp: number): TimestampState => ({
     tp: 'timestamp-state',
-    timestamp: timestamp % (24 * 60 * 60 * 1000)
+    timestamp: timestamp % MAX_TIMESTAMP
 });
 
 
 // Formatting timestamp -> time string
-const FORMAT_FROM_TOKEN: { [index: string]: (ms: number) => string } = {
-    H: ms => padTo2Digits(Math.floor(ms / TOKEN_TO_MS.H) % 24),
-    m: ms => ':' + padTo2Digits(Math.floor(ms / TOKEN_TO_MS.m) % 60),
-    s: ms => ':' + padTo2Digits(Math.floor(ms / TOKEN_TO_MS.s) % 60),
-    S: ms => '.' + (ms % TOKEN_TO_MS.S).toString().padStart(3, '0'),
-    '00': ms => ':00'
+const FORMAT_TO_TOKEN: { [index: string]: (ms: number) => string } = {
+    H: ms => padTo2Digits(Math.floor(ms / TOKEN_DATA.H.ms) % 24),
+    m: ms => ':' + padTo2Digits(getMinutes(ms)),
+    s: ms => ':' + padTo2Digits(getSeconds(ms)),
+    S: ms => '.' + getMilliseconds(ms).toString().padStart(3, '0')
 }
 
 const padTo2Digits = (num: number) =>  num.toString().padStart(2, '0');
 
 function formatTimestamp(milliseconds: number, pattern: string) {
-    const formattedParts = Object.keys(FORMAT_FROM_TOKEN).reduce((acc: string[], token) => pattern.includes(token) 
-            ? [...acc, FORMAT_FROM_TOKEN[token](milliseconds)] 
+    const formattedParts = TIME_TOKENS.reduce((acc: string[], token) => pattern.includes(token) 
+            ? [...acc, FORMAT_TO_TOKEN[token](milliseconds)] 
             : acc, []);
     return formattedParts.join('');
 }
 
 
 // Time parsing functionality
-const TIME_TOKENS: (keyof TimeToken)[] = ['H', 'm', 's', 'S'];
-const TOKEN_TO_MS: { [index: string]: number } = {
-    H: 60 * 60 * 1000,
-    m: 60 * 1000,
-    s: 1000,
-    S: 1
-}
-
 function parseStringToTime(value: string, pattern: string): number | undefined {
     const tokens = tokenizeString(value, true);
     if (tokens.length > 0) {
@@ -66,88 +96,40 @@ function parseStringToTime(value: string, pattern: string): number | undefined {
     }
 }
 
-function timeTokenToMs(usedTokens: (keyof TimeToken)[], timeToken: TimeToken, numberTokens?: NumberToken[]) {
+function timeTokenToMs(usedTokens: string[], timeToken: TimeToken, numberTokens?: NumberToken[]) {
     return usedTokens.reduce((acc: number, token) => {
-        const tokenValue = (timeToken[token] as number | undefined) ?? numberTokens?.shift()?.value ?? 0;
-        return acc + tokenValue * TOKEN_TO_MS[token];
+        const tokenValue = (timeToken[token as keyof TimeToken] as number) ?? numberTokens?.shift()?.value ?? 0;
+        return acc + tokenValue * TOKEN_DATA[token].ms;
     }, 0);
 }
 
-/*
-interface NumberToken {
-    type: 'number'
-    value: number
-    length: number
-}
 
-const number: RegExpExtractor = {
-    regex: /^([0-9]+)/,
-    extractor: match => ({type: 'number', value: parseInt(match[0]) || 0, length: match[0].length})
-}
-
-const Hm: RegExpExtractor = {
-    regex: /^(\d{1,2}):(\d{1,2})/,
-    extractor: match => ({
-        type: 'time',
-        H: parseInt(match[1]) || 0,
-        m: parseInt(match[2]) || 0,
-        length: match[0].length
-    })
-}
-
-const Hms: RegExpExtractor = {
-    regex: /^(\d{1,2}):(\d{1,2}):(\d{1,2})/,
-    extractor: match => ({
-        type: 'time',
-        H: parseInt(match[1]) || 0,
-        m: parseInt(match[2]) || 0,
-        s: parseInt(match[3]) || 0,
-        length: match[0].length
-    })
-}
-
-const HmsS: RegExpExtractor = {
-    regex: /^(\d{1,2}):(\d{1,2}):(\d{1,2})\.(\d{1,3})/,
-    extractor: match => ({
-        type: 'time',
-        H: parseInt(match[1]) || 0,
-        m: parseInt(match[2]) || 0,
-        s: parseInt(match[3]) || 0,
-        S: parseInt(match[4]) || 0,
-        length: match[0].length
-    })
-}
-/*
-const timeExtractors = [HmsS, Hms, Hm, number];
-
-function tokenizeString(value: string) {
-    let currentValue = value;
-    const tokens: Token[] = [];
-
-    while (currentValue.length > 0) {
-        const tokenOpt = tryRegexes(currentValue);
-        if (tokenOpt) {
-            tokens.push(tokenOpt);
-            currentValue = currentValue.slice(tokenOpt.length);
-        } else {
-            currentValue = currentValue.slice(1);
-        }
+// Arrows control logic
+function getAdjustedTime(timestamp: number, currentFMTChar: string, cycleThroughout: boolean, increment: number) {
+    const getCycleAdjustment = () => {
+        const { ms, max, get } = TOKEN_DATA[currentFMTChar];
+        const currValue = get(timestamp);
+        return ms * ((currValue + increment + max) % max - currValue);
     }
-    return tokens;
+    const adjustment = cycleThroughout
+        ? getCycleAdjustment()
+        : increment * TOKEN_DATA[currentFMTChar].ms;
+    const adjustedTime = timestamp + adjustment;
+    return (MAX_TIMESTAMP + adjustedTime) % MAX_TIMESTAMP;
 }
 
-function tryRegexes(value: string) {
-    let i = 0
-    while (i < timeExtractors.length) {
-        const result = tryRegex(value, timeExtractors[i]);
-        if (result) return result;
-        i++;
-    }
+function getCurrentFMTChar(pattern: string, cursorPosition: number) {
+    for (const pos of [cursorPosition, cursorPosition - 1]) {
+        if (TIME_TOKENS.includes(pattern[pos])) return pattern[pos];
+    }            
 }
 
-function tryRegex(value: string, regexExtractor: RegExpExtractor) {
-    const matchResult = value.match(regexExtractor.regex);
-    return matchResult && regexExtractor.extractor(matchResult);
-}*/
-
-export { formatTimestamp, parseStringToTime, createInputChange, createTimestampChange, isInputState, TOKEN_TO_MS }
+export {
+    formatTimestamp,
+    parseStringToTime,
+    createInputChange,
+    createTimestampChange,
+    isInputState,
+    getCurrentFMTChar,
+    getAdjustedTime
+}
