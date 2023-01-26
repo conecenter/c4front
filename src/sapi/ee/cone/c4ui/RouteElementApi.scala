f653766b40a4574a753d6057be175a3713321411
Dmitry Nor <dn@cone.ee>
2023-01-26 12:20:58 +0200

    added tag FrontApp CU-2ytznjp

--- src/sapi/ee/cone/c4ui/RouteElementApi.scala
+++ src/sapi/ee/cone/c4ui/RouteElementApi.scala
@@ -5,7 +5,7 @@ import ee.cone.c4vdom.{Receiver, ToChildPair, c4el, c4tags, c4val}
 import ee.cone.core.ui.c4view._front.RoutePartData
 
 
-@c4tags trait RouteElementTags[C] {
+@c4tags("FrontApp") trait RouteElementTags[C] {
   @c4el("RouteElement") def routeElement(
     key: String,
     compact: Boolean = false,
