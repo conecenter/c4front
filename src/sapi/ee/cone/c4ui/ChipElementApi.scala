dd58dbf662351e03545c5d368373f68ce16519fc
Dmitry Nor <dn@cone.ee>
2023-02-07 12:20:18 +0200

    Revert "Restored routeElement after Idea deleted it for no reason"
    This reverts commit baa6509bd889c368a73aa9fe2d55c1894c35510f.

--- src/sapi/ee/cone/c4ui/ChipElementApi.scala
+++ src/sapi/ee/cone/c4ui/ChipElementApi.scala
@@ -16,10 +16,4 @@ import ee.cone.c4vdom.{Receiver, ToChildPair, c4el, c4tags}
     withDelete: Boolean = false,
     children: ViewRes = Nil,
   ): ToChildPair
-
-  @c4el("RouteElement") def routeElement(
-    key: String,
-    routeParts: ViewRes = Nil, // ChipElement
-    compact: Boolean = false,
-  ): ToChildPair
 }
