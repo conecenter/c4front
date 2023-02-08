e8db4e473dd004627108567fe14f988dce215e3b
Aleafan <aaf@cone.ee>
2023-02-08 09:53:50 +0100

    add user input exchange with server

--- src/extra/route-element.tsx
+++ src/extra/route-element.tsx
@@ -1,12 +1,13 @@
-import React, { ReactElement, useMemo, useRef, MouseEvent } from 'react';
+import React, { ReactElement, useMemo, useRef, KeyboardEvent } from 'react';
 import clsx from 'clsx';
-import { COPY_EVENT, CUT_EVENT, PASTE_EVENT, useExternalKeyboardControls } from './focus-module-interface';
+import { COPY_EVENT, CUT_EVENT, DELETE_EVENT, PASTE_EVENT, useExternalKeyboardControls } from './focus-module-interface';
 import { NoFocusContext } from './labeled-element';
 import { copyToClipboard } from './utils';
 import { useSync } from '../main/vdom-hooks';
 import { identityAt } from '../main/vdom-util';
 import { getPath, useFocusControl } from './focus-control';
 import { isInsidePopup } from './dom-utils';
+import { useAddEventListener } from './custom-hooks';
 
 const keyboardActionIdOf = identityAt('keyboardAction');
 
@@ -30,8 +31,15 @@ function RouteElement({identity, compact, routeParts}: RouteElementProps) {
     const [_, sendPatch] = useSync(keyboardActionIdOf(identity));
 
     // Event handlers
-    function preventFocusInsidePopup(e: MouseEvent) {
+    function preventFocusInsidePopup(e: React.MouseEvent) {
         if (isInsidePopup(e.target as HTMLElement)) e.preventDefault();
+    }
+
+    function sendKeyToServer(e: KeyboardEvent | CustomEvent<{key: string, vk?: boolean}>) {
+        e.stopPropagation();
+        const key = isKeyboardEvent(e) ? e.key : e.detail.key;
+        const isPrintableKey = /^[a-z0-9]$/i.test(key);
+        if (isPrintableKey) sendPatch({value: key, headers: {'x-r-input': ''}});
     }
 
     // External keyboard event handlers
@@ -41,21 +49,29 @@ function RouteElement({identity, compact, routeParts}: RouteElementProps) {
 		[CUT_EVENT]: copyRouteToClipboard
 	};
 
-	useExternalKeyboardControls(routeElemRef.current, customEventHandlers);
-
     function copyRouteToClipboard(e: CustomEvent) {
         e.stopPropagation();
         const wholeCode = routeParts.reduce((accum, elem) => accum + (elem.props?.text ?? ''), '');
 		copyToClipboard(wholeCode);
 	}
 
+	useExternalKeyboardControls(routeElemRef.current, customEventHandlers);
+    useAddEventListener(routeElemRef.current, DELETE_EVENT, sendKeyToServer, true);
+
     return (
-        <div ref={routeElemRef} {...focusHtml} className={className} onMouseDownCapture={preventFocusInsidePopup} >
+        <div ref={routeElemRef}
+             className={className}
+             {...focusHtml} 
+             onKeyDown={sendKeyToServer} 
+             onMouseDownCapture={preventFocusInsidePopup} 
+        >
             <NoFocusContext.Provider value={true} >
                 {routeParts}
             </NoFocusContext.Provider>
         </div>
     );
 }
+
+const isKeyboardEvent = (e: KeyboardEvent | CustomEvent): e is KeyboardEvent => e.type === 'keydown';
 
  export { RouteElement };
