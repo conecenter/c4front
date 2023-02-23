74b45d5c3fcccd9cb99cdc86a20065a4d69a491e
Aleafan <aaf@cone.ee>
2023-02-23 07:52:53 +0100

    refactor PopupManager (CU-861maqmy9)

--- src/extra/popup-elements/popup-manager.ts
+++ src/extra/popup-elements/popup-manager.ts
@@ -1,45 +1,57 @@
-import { createElement as $, createContext, useMemo, useRef, useState, useContext, useCallback } from "react";
-import type { ReactNode, Dispatch, SetStateAction } from "react";
-import { getPath } from "../focus-control";
+import { createElement as $, createContext, useMemo, useRef, useContext, useEffect, ReactNode } from "react";
+import { PathContext } from "../focus-control";
 import { SyncedPopup } from "./synced-popup";
 import { NewPopupElement } from "./popup-element";
 import { usePatchSync } from "../exchange/patch-sync";
-import { useSync } from "../../main/vdom-hooks";
 
-type PopupContext = [string, (change: string) => void, HTMLElement | undefined] | [];
+interface PopupContext {
+    isOpened: (path: string) => boolean,
+    toggle: (path: string) => void,
+    popupDrawer?: HTMLElement
+};
 
-const PopupContext = createContext<PopupContext>([]);
+const defaultPopupContext: PopupContext = {
+    isOpened: () => false,
+    toggle: () => undefined
+};
+
+const PopupContext = createContext(defaultPopupContext);
+
 
 interface PopupManager {
     identity: Object,
-    state: string,
+    popupPath: string,
     children: ReactNode
 }
 
-function PopupManager({identity, state, children}: PopupManager) {
-    //const [_, sendClosePopupPatch] = useSync(popupActionIdOf(identity)) as [Patch[], (patch: Patch) => void];
+function PopupManager({identity, popupPath = '', children}: PopupManager) {
     const { currentState: popup, sendFinalChange: setPopup } =
-        usePatchSync(identity, 'receiver', state, false, s => s, (ch: string) => ({value: ch}), p => p.value, (_, ch) => ch);
+        usePatchSync(identity, 'receiver', popupPath, false, s => s, (ch: string) => ({value: ch}), p => p.value, (_, ch) => ch);
 
+    // Popup drawer element ref for Portal
     const popupDrawerRef = useRef<HTMLElement | undefined>();
     const popupDrawer = $('div', {ref: popupDrawerRef});
 
-    const value: PopupContext = useMemo(() => [popup, setPopup, popupDrawerRef.current], [popup]);
+    // Popups closing on focus changes
+    const focusPath = useContext(PathContext);
+    useEffect(() => {
+        console.log({focusPath, popup})
+        if (popup && !focusPath.includes(popup)) {
+            // TODO: find closest ':popup' key from the end of focusPath and make it new popup,
+            setPopup('');
+        }
+    }, [popup, focusPath]);
+
+    const isOpened = (path: string) => popup.includes(path);
+    // TODO: find closest ':popup' key from the end of popup and make it new popup
+    const toggle = (path: string) => setPopup(isOpened(path) ? '' : path);
+
+    const value: PopupContext = useMemo(() => {
+        return { isOpened, toggle, popupDrawer: popupDrawerRef.current };
+    }, [popup]);
 
     return $(PopupContext.Provider, { value }, children, popupDrawer);
 }
 
-
-type PopupState = [boolean, (on: boolean) => void, HTMLElement | undefined]
-
-const usePopupState = (identity: Object): PopupState => {
-    const path = useMemo(() => getPath(identity), [identity]);
-    const [popup, setPopup, popupDrawer] = useContext(PopupContext);
-    const isOpened = useCallback((p?: string)=> p === path, [path]);
-    const setOpened = useCallback((on: boolean) => setPopup?.(on ? path : ''), [path]);
-    return [isOpened(popup), setOpened, popupDrawer];
-}
-
-
+export { PopupManager, PopupContext }
 export const popupComponents = { PopupManager, NewPopupElement, SyncedPopup };
-export { PopupManager, usePopupState }
