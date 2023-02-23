c094031f939a52f310a30a45f69703d18f10ed65
Aleafan <aaf@cone.ee>
2023-02-23 07:53:43 +0100

    refactor PopupManager (CU-861maqmy9)

--- src/extra/popup-elements/popup-element.tsx
+++ src/extra/popup-elements/popup-element.tsx
@@ -1,45 +1,34 @@
-import React, { ReactNode, useCallback, useLayoutEffect, useRef, useState } from 'react';
+import React, { ReactNode, useCallback, useContext, useLayoutEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { usePopupPos } from '../../main/popup';
 import { NoCaptionContext } from '../../main/vdom-hooks';
 import { useAddEventListener } from '../custom-hooks';
 import { elementHasFocus } from '../dom-utils';
-import { usePopupState } from './popup-manager';
+import { PopupContext, usePopupState } from './popup-manager';
 
 interface PopupElement {
     key?: string,
-    identity: Object,
     children?: ReactNode
 }
 
-const DEFAULT_IDENTITY = { key: 'popup-element' };
-
-function NewPopupElement({ identity = DEFAULT_IDENTITY, children }: PopupElement) {
+function NewPopupElement({ key = ':popup', children }: PopupElement) {
     const [popupElement,setPopupElement] = useState<HTMLDivElement | null>(null);
     
     const popupParent = useRef<HTMLElement | null>(null);
     const setPopupParent = useCallback((el: HTMLElement | null) => popupParent.current = el && el.parentElement, []);
 
     // Popup state
-    const [isOpened, toggle, popupDrawer] = usePopupState(identity);
+    const [isOpened, toggle, popupDrawer] = useContext(PopupContext);
 
     // Popup positioning
     const [popupStyle] = usePopupPos(popupElement, false, popupParent.current);
-    
-    // Popup closing
-    const handleBlur = (e: FocusEvent) => {
-		if (elementIsInsideElements(e.relatedTarget, [popupElement, popupParent.current])) return;
-        toggle(false);
-	};
-    const doc = popupElement?.ownerDocument;
-    useAddEventListener(doc, 'focusout', handleBlur);
 
     // Prevent focus loss after closing
     useLayoutEffect(() => {
         return () => {
-            if (isOpened && elementHasFocus(popupDrawer)) focusFocusableAncestor(popupParent.current);
+            if (elementHasFocus(popupDrawer)) focusFocusableAncestor(popupParent.current);
         }
-    }, [isOpened]);
+    }, []);
 
     const popup = (
         <div ref={setPopupElement} className='popupEl' style={popupStyle} tabIndex={-1} >
@@ -49,7 +38,7 @@ function NewPopupElement({ identity = DEFAULT_IDENTITY, children }: PopupElement
 
     return (
         <NoCaptionContext.Provider value={false} >
-            {isOpened && popupDrawer ? createPortal(popup, popupDrawer) : null}
+            {popupDrawer ? createPortal(popup, popupDrawer) : null}
             <span ref={setPopupParent} style={{display: 'none'}}></span>
         </NoCaptionContext.Provider>
     );
