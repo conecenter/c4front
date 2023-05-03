3ddb0abee32f669def40534f8695677303cb1bba
Aleafan <aaf@cone.ee>
2023-05-03 09:35:42 +0200

    add server exchange for PrintManager; add print from server

--- src/extra/print-manager.tsx
+++ src/extra/print-manager.tsx
@@ -1,31 +1,52 @@
-import React, { ReactNode, createContext, useRef, useState } from "react";
+import React, { ReactNode, createContext, useEffect, useState } from "react";
 import { useAddEventListener } from "./custom-hooks";
+import { usePatchSync } from "./exchange/patch-sync";
 
 const PrintContext = createContext(false);
 PrintContext.displayName = 'PrintContext';
 
 interface PrintManager {
     key: string,
+    identity: Object,
     children: ReactNode,
     printChildren: ReactNode,
     printMode: boolean
 }
 
-function PrintManager({ children, printMode, printChildren }: PrintManager) {
-    const windowRef = useRef<Window | null | undefined>(null);
-    const [print, setPrint] = useState(false);
-    
-    useAddEventListener(windowRef.current, 'beforeprint', () => setPrint(true))
-    useAddEventListener(windowRef.current, 'afterprint', () => setPrint(false))
+const changeToPatch = () => ({
+    headers: {"x-r-printMode": "0"},
+    value: ""
+});
+
+function PrintManager({ identity, children, printMode: state, printChildren }: PrintManager) {
+    const [elem, setElem] = useState<HTMLDivElement | null>(null);
+    const window = elem?.ownerDocument.defaultView;
+
+    const [isPrinting, setIsPrinting] = useState(false);
+
+    const {currentState: printMode, sendFinalChange} =
+        usePatchSync(identity, 'receiver', state, false, (b) => b, changeToPatch, (p) => false, (prev, ch) => ch);
+
+    // Custom print from server
+    useEffect(() => {
+        if (printMode) window?.print();
+    }, [printMode]);
+
+    // Make changes for print
+    useAddEventListener(window, 'beforeprint', () => setIsPrinting(true));
+    useAddEventListener(window, 'afterprint', onAfterPrint);
+
+    function onAfterPrint() {
+        setIsPrinting(false);
+        printMode && sendFinalChange(false);
+    };
 
     return (
         <>
-            {<div ref={elem => windowRef.current = elem?.ownerDocument.defaultView} className='mainContent'>
-                {children}
-            </div>}
-            {print && (
+            <div ref={setElem} className='mainContent'>{children}</div>
+            {isPrinting && (
                 <div className='printContent'>
-                    <PrintContext.Provider value={true} children={printChildren} />
+                    <PrintContext.Provider value={true} children={printMode ? printChildren : children} />
                 </div>
             )}
         </>
