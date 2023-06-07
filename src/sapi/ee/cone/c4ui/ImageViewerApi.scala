63138a14a60cf87210afaac54618ccdb36d32751
Dmitry Nor <thepineoak@gmail.com>
2023-06-07 13:00:57 +0200

    added Position to ImageViewer CU-861mt8ktz

--- src/sapi/ee/cone/c4ui/ImageViewerApi.scala
+++ src/sapi/ee/cone/c4ui/ImageViewerApi.scala
@@ -3,8 +3,13 @@ package ee.cone.c4ui
 import ee.cone.c4vdom.{Receiver, ToChildPair, ToJson, c4el, c4tagSwitch, c4tags, c4val}
 
 @c4tagSwitch("FrontApp") trait Slide extends ToJson
+@c4tagSwitch("FrontApp") trait Position extends ToJson
 
 @c4tags("FrontApp") trait ImageViewerTags[C] {
+  @c4val("fullscreen") def fullscreen: Position
+
+  @c4val("inline") def inline: Position
+
   @c4val def slide(
     src: String,
     title: String = "",
@@ -14,6 +19,7 @@ import ee.cone.c4vdom.{Receiver, ToChildPair, ToJson, c4el, c4tagSwitch, c4tags,
     key: String,
     index: Int,
     slides: List[Slide] = Nil,
+    position: Position = fullscreen,
     slideChange: Receiver[C] = NoReceiver[C]
   ): ToChildPair
 }
