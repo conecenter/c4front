9fada3276ceb392a8623c26198d338f9fe07052d
Aleafan <aaf@cone.ee>
2023-07-04 08:56:20 +0200

    add VkInfoContext & functionality

--- src/extra/ui-info-provider.ts
+++ src/extra/ui-info-provider.ts
@@ -1,4 +1,4 @@
-import { createElement as $, ReactNode, createContext, useContext, useLayoutEffect, useRef } from 'react';
+import { createElement as $, ReactNode, createContext, useContext, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { usePatchSync, Patch } from "./exchange/patch-sync";
 import { useAddEventListener } from "./custom-hooks";
 import { RootBranchContext } from '../main/vdom-hooks';
@@ -9,6 +9,15 @@ type UiType = 'pointer' | 'touch';
 
 const UiInfoContext = createContext<UiType>(DEFAULT_UI_TYPE);
 UiInfoContext.displayName = 'UiInfoContext';
+
+
+interface VkInfoContext {
+    haveVk: boolean,
+    setHaveVk?: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const VkInfoContext = createContext<VkInfoContext>({ haveVk: false });
+VkInfoContext.displayName = 'VkInfoContext';
 
 
 // Server exchange
@@ -28,6 +37,7 @@ interface UiInfoProvider {
 }
 
 function UiInfoProvider({identity, uiType: state, children}: UiInfoProvider) {
+    // UiType functionality
     const {currentState: uiType, sendFinalChange} =
         usePatchSync(identity, 'receiver', state, false, (b) => b, changeToPatch, patchToChange, (prev, ch) => ch);
 
@@ -45,7 +55,12 @@ function UiInfoProvider({identity, uiType: state, children}: UiInfoProvider) {
 
     useAddEventListener(pointerMql.current, 'change', updateUiType);
 
-    return $(UiInfoContext.Provider, {value: uiType || DEFAULT_UI_TYPE}, children)
+    // VK context functionality
+    const [haveVk, setHaveVk] = useState(false);
+    const vkInfo = useMemo(() => ({ haveVk, setHaveVk }), [haveVk]);
+
+    return $(UiInfoContext.Provider, {value: uiType || DEFAULT_UI_TYPE}, 
+        $(VkInfoContext.Provider, {value: vkInfo}, children));
 }
 
-export { UiInfoContext, UiInfoProvider }
+export { UiInfoContext, VkInfoContext, UiInfoProvider }
