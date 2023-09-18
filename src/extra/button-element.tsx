fc9b8f568c0528117daca277a0e049b9ea78450c
Aleafan <aaf@cone.ee>
2023-09-18 11:18:55 +0200

    refactor ButtonElement (CU-862kd2hzc)

--- src/extra/button-element.tsx
+++ src/extra/button-element.tsx
@@ -1,80 +1,86 @@
-import React, { createElement as $ } from 'react';
+import React, { createElement as $, ReactNode, MutableRefObject } from 'react';
 import clsx from 'clsx';
 import { findFirstParent } from "../main/vdom-util";
 import { useFocusControl } from './focus-control';
+import { Patch } from './exchange/patch-sync';
 
-const ButtonElement = (props) => {
-	const elem = React.useRef(null)
-	const changing = !!props.value	/*changing*/
+interface ButtonElement {
+    value: '1' | '',
+    onClick?: (e: React.MouseEvent) => void,
+    onChange?: (e: { target: Patch }) => void,
+    path: string,
+    content?: string,
+    children?: ReactNode,
+    disabled?: boolean,
+    marker?: string,
+    url?: string,
+    hint?: string,
+	className?: string,
+    forwardRef?: MutableRefObject<HTMLButtonElement | null>
+}
+
+const ButtonElement = (props: ButtonElement) => {
+	const elem = React.useRef<HTMLButtonElement | null>(null)
+
+	const changing = !!props.value
 	const disabled = props.disabled || changing
 	const noAction = !(props.onClick || props.onChange)
-	const style = { ...disabled && { opacity: "0.4" } }
+
 	const { focusClass, focusHtml } = useFocusControl(props.path)
+
 	const markerClass = props.marker && `marker-${props.marker}`
-	const className = clsx(props.className, focusClass, noAction && 'noAction', markerClass)
+
 	React.useEffect(() => {
 		if (props.forwardRef) props.forwardRef.current = elem.current
-		elem.current.changing = changing
 	}, [changing])
-	/*
-	const create = el=>(type,params) => {
-		const doc =  (el.ownerDocument || el.documentElement.ownerDocument)
-		w = getParentW(doc.defaultView)					
-		const _params = params && (params.code =="vk" && type =="keydown")?{...params,detail:params}:params //ie11 hack
-		return (new w.CustomEvent(type,_params))
-	}	
-	*/
+
+    const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (!disabled && (props.onClick || props.onChange)) {
+            e.stopPropagation()
+			if (props.onClick) props.onClick(e)
+			else props.onChange?.({ target: { headers: { "x-r-action": "change" }, value: "1" } })
+        }
+        if (props.url) {
+            e.stopPropagation()
+            e.preventDefault()
+            window.open(props.url)
+        }
+        const focEl = findFirstParent((el: HTMLElement) => el.classList.contains("activeFocusWrapper") && el)(elem.current)
+        if (focEl) focEl.focus()
+        // problem was dropdown in popup: button-option disappear after click, and focus goes to nowhere, and popup closes
+    }
+
 	React.useEffect(() => {
-		const onEnter = e => {
+		const onEnter = (e: CustomEvent) => {
 			e.stopPropagation()
-			elem.current.click()
-			setTimeout(function () {
-				const cEvent = eventManager.create(e.target)("cTab", { bubbles: true })
-				elem.current.dispatchEvent(cEvent)
-			}, 200)
+			elem.current?.click()
+			const cEvent = createCustomEvent('cTab', elem.current)
+			if (cEvent) elem.current?.dispatchEvent(cEvent)
 		}
-		const onClick = e => {
-			if (!disabled && (props.onClick || props.onChange)) {
-				e.stopPropagation()
-				const w = e.target.ownerDocument.defaultView
-				w.setTimeout(() => {
-					if (props.onClick) props.onClick(e)
-					else if (props.onChange) props.onChange({ target: { headers: { "x-r-action": "change" }, value: "1" } })
-				}, (props.delay ? parseInt(props.delay) : 0))
-			}
-			if(props.url) {
-                e.stopPropagation()
-                e.preventDefault()
-                window.open(props.url)
-            }
-			const focEl = findFirstParent(el => el.classList.contains("activeFocusWrapper") && el)(elem.current)
-			if (focEl) focEl.focus()
-			// problem was dropdown in popup: button-option disappear after click, and focus goes to nowhere, and popup closes
-		}
-		elem.current.addEventListener("enter", onEnter)
-		elem.current.addEventListener("click", onClick)
+		elem.current?.addEventListener("enter", onEnter)
 		return () => {
-			elem.current.removeEventListener("enter", onEnter)
-			elem.current.removeEventListener("click", onClick)
+			elem.current?.removeEventListener("enter", onEnter)
 		}
 	}, [props.onClick, props.onChange, changing])
-	const onMouseOver = (value) => () => {
-		if (value) props.onMouseOver && props.onMouseOver()
-		else props.onMouseOut && props.onMouseOut()
-	}
 
 	const textContent = props.content && $('span', { className: 'text' }, props.content)
 	const children = props.children !== props.content && props.children
 
 	return $("button", {
 			key: "btn", ref: elem,
-			title: props.hint, className,
-			style, ...focusHtml,
-			onMouseOver: onMouseOver(true), onMouseOut: onMouseOver(false),
+            onClick, title: props.hint, ...focusHtml,
+            className: clsx(props.className, focusClass, noAction && 'noAction', markerClass),
+			style: disabled ? { opacity: "0.4" } : undefined,
+			onKeyDown: (e) => e.preventDefault()
 		},
 		textContent,
 		children
 	)
+}
+
+function createCustomEvent(name: string, elem: HTMLElement | null) {
+	const window = elem?.ownerDocument.defaultView;
+	return window ? new window.CustomEvent(name, { bubbles: true }) : undefined;
 }
 
 export { ButtonElement }
