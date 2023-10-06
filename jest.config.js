c0ee9bb7e15ac1644a814efc63c1b398b22ec1a8
Aleafan <aaf@cone.ee>
2023-10-06 08:31:11 +0200

    fix files transpilation bug & speed for jest with @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
 	testEnvironment: "jsdom",
 	transform: {
-		"^.+\\.tsx?$": "ts-jest"
+		'^.+\\.jsx?$': ['@swc/jest', {
+			"jsc": { "parser": { "jsx": true } }
+		}],
+		'^.+\\.tsx?$': ['@swc/jest', {
+			"jsc": { "parser": { "syntax": "typescript", "tsx": true } }
+		}]
 	},
-	setupFilesAfterEnv: [
-		'<rootDir>/src/test/config/setup-jest.ts'
-	]
+	setupFilesAfterEnv: ["<rootDir>/src/test/config/setup-jest.ts"],
+	extensionsToTreatAsEsm: [".ts", ".tsx"]
 }
