e4ef34e6fba02508077dc9069d4e6f4d988123c3
Aleafan <aaf@cone.ee>
2023-12-21 13:18:12 +0100

    add timezone logic

--- src/extra/calendar.tsx
+++ src/extra/calendar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import luxon3Plugin from '@fullcalendar/luxon3';
+import { useUserLocale } from './locale';
 
 interface Calendar {
     events: CalendarEvent[]
@@ -15,12 +17,15 @@ interface CalendarEvent {
 }
 
 function Calendar({ events }: Calendar) {
+    const locale = useUserLocale();
+
     return (
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, luxon3Plugin]}
         initialView="dayGridMonth"
         firstDay={1}
         slotDuration={'00:15'}
+        timeZone={locale.timezoneId}
         headerToolbar={{
             left: 'prev,next today',
             center: 'title',
