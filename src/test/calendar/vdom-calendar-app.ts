b5952007da0ddf17ef4631c472e8fe941676c58a
Aleafan <aaf@cone.ee>
2023-12-26 15:48:01 +0100

    refactor calendar app & add popup

--- src/test/calendar/vdom-calendar-app.ts
+++ src/test/calendar/vdom-calendar-app.ts
@@ -3,60 +3,64 @@ import { createElement as $ } from "react";
 import { createSyncProviders } from "../../main/vdom-hooks";
 import { Calendar } from "../../extra/calendar";
 import { ButtonElement } from "../../extra/button-element";
+import { SyncedPopup } from "../../extra/popup-elements/synced-popup";
+import { PopupManager } from "../../extra/popup-elements/popup-manager";
 
 function App() {
-    const eventChild = $(ButtonElement,
-        { value: '', className: 'yellowColorCss', onClick: () => console.log('clicked') },
-        'Full calendar'
+    const eventChild = [
+        $(ButtonElement, {key: '1', value: '', path: '/button', className: 'yellowColorCss', onClick: () => console.log('clicked')}, 'Full calendar'),
+        $(SyncedPopup, {key: '2', identity: { key: 'popup' }}, 'Hello world')
+    ];
+
+    const calendar = $(Calendar, {
+        identity: { key: 'test' },
+        businessHours: {
+            daysOfWeek: [1, 2, 3, 4, 5],
+            startTime: 28800000,
+            endTime: 64800000
+        },
+        allDaySlot: false,
+        events: [
+            {
+                id: 'Meeting',
+                title: 'Meeting',
+                start: new Date(2023, 11, 20, 12).getTime(),
+                end: new Date(2023, 11, 20, 14).getTime(),
+                color: { tp: 'p', cssClass: 'primaryColorCss' },
+                children: eventChild
+            },
+            {
+                id: 'Meeting-2',
+                title: 'Meeting-2',
+                start: new Date(2023, 11, 21, 14).getTime(),
+                end: new Date(2023, 11, 21, 16).getTime()
+            },
+            {
+                id: 'Meeting-3',
+                title: 'Meeting-3',
+                start: new Date(2023, 11, 21, 14).getTime(),
+                end: new Date(2023, 11, 21, 16).getTime()
+            },
+            {
+                id: 'Car arrival',
+                title: 'Car arrival',
+                start: new Date(2023, 11, 21, 10).getTime(),
+                end: new Date(2023, 11, 21, 14).getTime(),
+                color: { tp: 'r', bgColor: 'grey', textColor: 'white' }
+            },
+            {
+                id: 'Train departure',
+                title: 'Train departure',
+                start: new Date(2023, 11, 23).getTime(),
+                allDay: true
+            }
+        ]
+    });
+
+    const children = $(PopupManager, null,
+        $('div', {style: { paddingLeft: '4em', width: '1000px', maxWidth: '100%' }}, calendar)
     );
 
-    const children = $('div', { style: { paddingLeft: '4em', width: '1000px', maxWidth: '100%' } },
-        $(Calendar, {
-            identity: { key: 'test' },
-            businessHours: {
-                daysOfWeek: [1, 2, 3, 4, 5],
-                startTime: 28800000,
-                endTime: 64800000
-            },
-            allDaySlot: false,
-            events: [
-                {
-                    id: 'Meeting',
-                    title: 'Meeting',
-                    start: new Date(2023, 11, 24, 12).getTime(),
-                    end: new Date(2023, 11, 24, 14).getTime(),
-                    color: { tp: 'p', cssClass: 'primaryColorCss' },
-                    children: eventChild
-                },
-                {
-                    id: 'Meeting-2',
-                    title: 'Meeting-2',
-                    start: new Date(2023, 11, 22, 14).getTime(),
-                    end: new Date(2023, 11, 22, 16).getTime(),
-                    
-                },
-                {
-                    id: 'Meeting-3',
-                    title: 'Meeting-3',
-                    start: new Date(2023, 11, 24, 14).getTime(),
-                    end: new Date(2023, 11, 24, 16).getTime()
-                },
-                {
-                    id: 'Car arrival',
-                    title: 'Car arrival',
-                    start: new Date(2023, 11, 24, 10).getTime(),
-                    end: new Date(2023, 11, 24, 14).getTime(),
-                    color: { tp: 'r', bgColor: 'grey', textColor: 'white' }
-                },
-                {
-                    id: 'Train departure',
-                    title: 'Train departure',
-                    start: new Date(2023, 11, 23).getTime(),
-                    allDay: true
-                }
-            ]
-        })
-    );
     const sender = {
         enqueue: (identity: any, patch: any) => console.log(patch),
         ctxToPath: () => '/test'
