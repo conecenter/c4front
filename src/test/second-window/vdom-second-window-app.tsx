5fe2ecd6db3f626eaa3f06564a4c1a73ce7faa5c
Aleafan <aaf@cone.ee>
2024-03-21 09:43:17 +0100

    add rendering 2 different pieces of UI in second window via react-new-window patch

--- src/test/second-window/vdom-second-window-app.tsx
+++ src/test/second-window/vdom-second-window-app.tsx
@@ -1,21 +1,30 @@
 import ReactDOM from "react-dom";
-import React, {createElement as $} from "react";
+import React, {createElement as $, useState} from "react";
 import {createSyncProviders} from "../../main/vdom-hooks";
 import {SecondWindowButton, SecondWindowComponent, SecondWindowManager} from "../../extra/second-window";
 
 function App() {    
+	const [showContent, setShowContent] = useState(false);
     const children = (
       <SecondWindowManager>
         <h1>Second Window!</h1>
+
         <SecondWindowComponent>
-          <p>Cone Center</p>
+			<p>Cone Center</p>
+			<img src='../images/denmark.svg' style={{ width: '5em' }} />
         </SecondWindowComponent>
 
         <SecondWindowButton
-          value=''
-          path='/second-monitor-btn'
-          content='Open Second Window'
-          className='primaryColorCss' />
+			value=''
+			path='/second-monitor-btn'
+			content='Open Second Window'
+			className='primaryColorCss' />
+
+		<button onClick={() => setShowContent(true)}>Show content</button>
+        {showContent && <SecondWindowComponent>
+			<h2>Denmark</h2>
+			<img src='../images/denmark.svg' style={{ width: '5em' }} />
+		</SecondWindowComponent>}
       </SecondWindowManager>
     );
 
