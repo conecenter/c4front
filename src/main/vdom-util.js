e54f8c19527f9285fd8a8e97f15fb28fb1aa062f
sk <rewlad@gmail.com>
2024-12-15 20:16:18 +0200

    sk-ws

--- src/main/vdom-util.js
+++ src/main/vdom-util.js
@@ -13,7 +13,10 @@ export const weakCache = f => {
     }
 }
 
-export const identityAt = key => weakCache(parent => ({ parent, key }))
+export const identityAt = key => {
+    const cache = weakCache(parent => ({ parent, key }))
+    return parent => typeof parent === "string" ? parent+'/'+key : cache(parent)
+}
 
 export const receiverOf = identityAt('receiver')
 
