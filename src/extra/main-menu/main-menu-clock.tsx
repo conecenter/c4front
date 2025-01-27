fbc69c1cefc7229126998abb85362c3a517f7a41
Aleafan <aaf@cone.ee>
2025-01-27 13:37:52 +0100

    fix MainMenuClock copies syncing in parallel

--- src/extra/main-menu/main-menu-clock.tsx
+++ src/extra/main-menu/main-menu-clock.tsx
@@ -1,13 +1,14 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import clsx from 'clsx';
 import {formatInTimeZone} from 'date-fns-tz'
 import {identityAt} from '../../main/vdom-util';
 import {useSync} from '../../main/vdom-hooks';
-import {Patch} from '../exchange/input-sync';
 import {useUserLocale} from '../locale';
 import {bg, de, da, et, enGB, lt, pl, ro, ru, uk, it} from 'date-fns/locale';
 import {useFocusControl} from '../focus-control';
+import {VISIBLE_CHILD_SELECTOR} from '../css-selectors';
 import type { Locale } from 'date-fns'
+import type { SendPatch } from '../exchange/patch-sync';
 
 interface IntlLocales {
   [name: string]: Locale
@@ -47,16 +48,22 @@ function MainMenuClock({identity, serverTime, timestampFormatId, path}: MainMenu
   const [date, time] = formattedDate.split('|');
 
   // Time sync with server
+  const ref = useRef<HTMLDivElement>(null);
   const timeSyncIdOf = identityAt('timeSync');
-  const [_, enqueueTimeSyncPatch] = useSync(timeSyncIdOf(identity)) as [Patch[], (patch: Patch) => void];
+  const [_, enqueueTimeSyncPatch] = useSync(timeSyncIdOf(identity)) as [SendPatch[], (patch: SendPatch) => void];
 
-  const syncWithServer = () => enqueueTimeSyncPatch({value: '1'});
+  const syncWithServer = useCallback(() => setTimeout(() => {
+      // menu have multiple copies of elements - hidden & visible
+      const isVisible = ref.current?.matches(VISIBLE_CHILD_SELECTOR);
+      isVisible && enqueueTimeSyncPatch({value: '1'});
+    }), [enqueueTimeSyncPatch]
+  );
 
   useEffect(() => {
     syncWithServer();
     const id = setInterval(syncWithServer, SYNC_INTERVAL);
     return () => clearInterval(id);
-  }, []);
+  }, [syncWithServer]);
 
   // Offset correction after server sync
   useEffect(() => {
@@ -76,7 +83,8 @@ function MainMenuClock({identity, serverTime, timestampFormatId, path}: MainMenu
   const { focusClass, focusHtml } = useFocusControl(path);
 
   return (
-    <div 
+    <div
+      ref={ref}
       style={isSynced ? undefined : {visibility: 'hidden'}} 
       className={clsx('menuCustomItem dateTimeClock', focusClass)}
       {...focusHtml}
