291f2953aeaf375db7b695d1c1f35497f71d6918
Sergei Kirjanov <rewlad@gmail.com>
2025-02-11 14:39:54 +0200

    busyFor

--- src/extra/sender-busy-notifier.ts
+++ src/extra/sender-busy-notifier.ts
@@ -6,25 +6,10 @@ import { OverlayMessage } from "./overlay-manager";
 const BUSY_OVERLAY = { id: 'busyFor', priority: 1, message: "Calculating\nPlease wait a moment" };
 
 function SenderBusyNotifier() {
-	const [isBusy, setIsBusy] = useState(false);
-	const busyFrom = useRef(0);
-
-	const { isBusy: isSenderBusy } = useSender();
-	const ack = useContext(AckContext);
-
-	const checkBusy = () => {
-		if (isSenderBusy(ack)) {
-			if (!busyFrom.current) busyFrom.current = Date.now();
-			else setIsBusy(Date.now() - busyFrom.current > 3000);
-		}
-		else {
-			busyFrom.current = 0;
-			setIsBusy(false);
-		}
-	}
-	useInterval(checkBusy, 500);
-
-	return isBusy ? $(OverlayMessage, BUSY_OVERLAY) : null;
+	const [isBusy, setIsBusy] = useState(false)
+	const {busyFor} = useSender()
+	useInterval(() => setIsBusy(busyFor() > 5000), 500)
+	return isBusy ? $(OverlayMessage, BUSY_OVERLAY) : null
 }
 
 export { SenderBusyNotifier }
