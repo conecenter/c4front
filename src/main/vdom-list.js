e71c8826f93d9c0d0331b907c01d61332065fc1e
Aleafan <aaf@cone.ee>
2025-04-09 12:11:53 +0200

    fix horizontal ctx in lists inside groupBoxes

--- src/main/vdom-list.js
+++ src/main/vdom-list.js
@@ -2,7 +2,7 @@ import {cloneElement, createElement as $, useCallback, useEffect, useMemo, useSt
 import clsx from 'clsx'
 
 import {findFirstParent, identityAt, never, sortedWith} from "./vdom-util.js"
-import {NoCaptionContext, RootBranchContext, useEventListener, useSync, usePath} from "./vdom-hooks.js"
+import {NoCaptionContext, HorizontalCaptionContext, RootBranchContext, useEventListener, useSync, usePath} from "./vdom-hooks.js"
 import {useWidth,useMergeRef} from "./sizes.js"
 import {useGridDrag} from "./grid-drag.js"
 import {ESCAPE_KEY} from "./keyboard-keys"
@@ -323,10 +323,9 @@ export function GridRoot({ identity, rows: argRows, cols: argCols, children: raw
     const dragCSSEl = $("style",{dangerouslySetInnerHTML: { __html: dragCSSContent}})
 
     return $(NoCaptionContext.Provider,{value:true},
-        $(InputsSizeContext.Provider,{value: fixedCellsSize ? 50 : 25},
-            $(BindGroupElement,{groupId:'grid-list-bind'},dragCSSEl,res)
-        )
-    )
+        $(HorizontalCaptionContext.Provider, {value: false},
+            $(InputsSizeContext.Provider,{value: fixedCellsSize ? 50 : 25},
+                $(BindGroupElement,{groupId:'grid-list-bind'},dragCSSEl,res))))
 }
 
 const getAllChildren = ({children,rows,cols,hasHiddenCols,alwaysShowExpander,hideElementsForHiddenCols,dragRowKey}) => {
