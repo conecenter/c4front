de6166891d46d215190a3987888446e644b4e38a
Aleafan <aaf@cone.ee>
2025-10-21 12:05:10 +0200

    add focus frame to EventContent (DE-13634)

--- src/extra/calendar/event-content.tsx
+++ src/extra/calendar/event-content.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react';
+import clsx from 'clsx';
 import { EventContentArg } from '@fullcalendar/core';
+import { useFocusControl } from '../focus-control';
 
 interface EventContent {
     eventInfo: EventContentArg,
@@ -8,8 +10,13 @@ interface EventContent {
 }
 
 function EventContent({ eventInfo, customContent, onEventClick }: EventContent) {
+    const { focusClass, focusHtml } = useFocusControl(eventInfo.event.id);
     return (
-        <div onClick={() => onEventClick(eventInfo.event.id)} tabIndex={-1} className="fc-event-main-frame">
+        <div
+            onClick={() => onEventClick(eventInfo.event.id)}
+            className={clsx("fc-event-main-frame", focusClass)}
+            {...focusHtml}
+        >
             <div className="fc-event-time">{eventInfo.timeText}</div>
             <div className="fc-event-title-container">
                 <div className="fc-event-title fc-sticky">{eventInfo.event.title}</div>
