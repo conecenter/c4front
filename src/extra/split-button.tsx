00cb16fc32f1b52c95886b527d11f42726a58d37
Aleafan <aaf@cone.ee>
2025-10-30 13:03:07 +0100

    add color + focus + NoCaption to SplitButton

--- src/extra/split-button.tsx
+++ src/extra/split-button.tsx
@@ -1,49 +1,57 @@
-import React, { ReactElement, ReactNode } from "react";
+import React, { ReactNode } from "react";
 import clsx from "clsx";
 import { ButtonElement } from "./button-element";
 import { SVGElement } from "../main/image";
-import { ColorProps, colorToProps } from "./view-builder/common-api";
-import { usePath } from "../main/vdom-hooks";
+import { ColorDef, ColorProps, colorToProps } from "./view-builder/common-api";
+import { NoCaptionContext, usePath } from "../main/vdom-hooks";
 import { usePopupState } from "./popup-elements/popup-manager";
 import { PopupElement } from "./popup-elements/popup-element";
+import { useFocusControl } from "./focus-control";
+
+const ARROW_DOWN_URL = '/mod/main/ee/cone/core/ui/c4view/arrow-down.svg';
 
 interface SplitButtonProps {
     identity: object,
-    mainButton: ReactElement<ButtonElement>,
+    mainButton: ReactNode[],
+    color: ColorDef,
     optionalGroup?: ReactNode[]
 }
 
-function SplitButton({ identity, mainButton, optionalGroup = [] }: SplitButtonProps) {
-    const { style: colorStyle, className: colorClass }: ColorProps = colorToProps(mainButton.props.color);
+function SplitButton({ identity, mainButton, color, optionalGroup = [] }: SplitButtonProps) {
+    const { style: colorStyle, className: colorClass }: ColorProps = colorToProps(color);
 
     const path = usePath(identity);
+
+    const { focusClass, focusHtml } = useFocusControl(path);
 
     const { isOpened, toggle } = usePopupState(path);
 
     return (
-        <div className={clsx("splitButton", colorClass)} style={colorStyle}>
-            {mainButton}
+        <div className={clsx("splitButton", colorClass, focusClass)} style={colorStyle} {...focusHtml} >
+            <NoCaptionContext.Provider value={true} >
+                {mainButton}
 
-            {optionalGroup.length > 0 && (
-                <>
-                    <div className="btnSeparator" />
+                {optionalGroup.length > 0 && (
+                    <>
+                        <div className="btnSeparator" />
 
-                    <ButtonElement
-                        className={clsx("arrowButton", isOpened && 'rotate180deg')}
-                        onClick={() => toggle(!isOpened)}
-                        value=''
-                    >
-                        <SVGElement url="./images/arrow-down.svg" color="adaptive" />
-                    </ButtonElement>
+                        <ButtonElement
+                            className={clsx("arrowButton", isOpened && 'rotate180deg')}
+                            onClick={() => toggle(!isOpened)}
+                            value=''
+                        >
+                            <SVGElement url={ARROW_DOWN_URL} color="adaptive" />
+                        </ButtonElement>
 
-                    {isOpened &&
-                        <PopupElement popupKey={path} className="splitBtnPopup" >
-                            {optionalGroup}
-                        </PopupElement>}
-                </>
-            )}
+                        {isOpened &&
+                            <PopupElement popupKey={path} className="splitBtnPopup" >
+                                {optionalGroup}
+                            </PopupElement>}
+                    </>
+                )}
+            </NoCaptionContext.Provider>
         </div>
-    )
+    );
 }
 
 export { SplitButton }
