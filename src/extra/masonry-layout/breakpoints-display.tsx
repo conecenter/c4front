39f4c2d796ee62b5fca23218a3a89f945b5e7c9b
Aleafan <aaf@cone.ee>
2025-12-12 13:33:29 +0100

    masonry preview: add bp selection + logic, scale indicator

--- src/extra/masonry-layout/breakpoints-display.tsx
+++ src/extra/masonry-layout/breakpoints-display.tsx
@@ -5,31 +5,40 @@ import { SegmentedChip } from "../segmented-chip";
 
 interface BreakpointsDisplay {
     breakpoints: { [P: string]: number },
-    currentBp: string | null
+    currentBp: string | null,
+    scale: number,
+    onBreakpointChange: (bp: string) => void
 }
 
-function BreakpointsDisplay({ breakpoints, currentBp }: BreakpointsDisplay) {
+function BreakpointsDisplay({ breakpoints, currentBp, scale, onBreakpointChange }: BreakpointsDisplay) {
     const bpChips = Object.entries(breakpoints)
         .sort((a, b) => a[1] - b[1])
         .map((bp, i) => (
             <ChipElement
                 key={`chip-${i}`}
                 identity={{}}
-                text={`${bp[0]}: ${bp[1]}`}
+                text={`${bp[0]}: >${bp[1]}px`}
+                onClick={() => bp[0] && onBreakpointChange(bp[0])}
                 color={{
                     tp: 'p',
                     cssClass: bp[0] === currentBp ? 'greenLighterColorCss' : 'grayLighterColorCss'}
                 } />));
     return (
-        <>
-            <LabeledElement label='Breakpoints:' className='masonryBps' sizes={{ min: 3, max: 10 }}>
+        <div className='masonryBps' >
+            <LabeledElement label='Breakpoints:' sizes={{ min: 3, max: 10 }}>
                 <SegmentedChip
                     identity={{ key: "segmented-chip" }}
                     compact={true}
                     routeParts={bpChips} />
             </LabeledElement>
-            <hr />
-        </>
+            <LabeledElement>
+                <ChipElement
+                    identity={{}}
+                    text={`Scale: ${scale * 100}%`}
+                    color={{ tp: 'p', cssClass: 'bodyColorCss' }}
+                />
+            </LabeledElement>
+        </div>
     )
 }
 
