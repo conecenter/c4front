ab55b7e153e00f54881938c4e9f23e619395fd6a
Aleafan <aaf@cone.ee>
2026-05-22 18:00:34 +0200

    WIP on InputElement tests (DE-23189)

--- src/test/input-element/input.test.jsx
+++ src/test/input-element/input.test.jsx
@@ -12,28 +12,45 @@ function InputWithSync({ value = "" }) {
     return <InputElement path="/input-element" {...patch} />
 }
 
-function App({ children }) {
-    const sender = {enqueue: jest.fn(), ctxToPath: () => '/test'}
+function App({ enqueue, children }) {
+    const sender = {enqueue: enqueue || jest.fn(), ctxToPath: () => '/test'}
     return createSyncProviders({sender, ack: null, isRoot: true, branchKey: 'abc', children});
 }
 
-const renderWithValue = (value) => {
+const renderWithProps = ({ value, enqueue }) => {
     render(
-      <App>
+      <App enqueue={enqueue}>
         <InputWithSync value={value} />
       </App>
     );
 }
 
-// 1) focus inside input, type 'abc', focus out -> should be 'abc' final value
-// 2) sends x-r-changing header while typing, drops it on blur
 describe('basic functionality', () => {
     it("input renders and text can be typed", async () => {
         const user = userEvent.setup();
-        renderWithValue();
+        renderWithProps({});
         const input = screen.getByRole('textbox');
         expect(input).toBeInTheDocument();
         await user.type(input, 'hello');
         expect(input).toHaveValue("hello");
+    });
+
+    it("sends x-r-changing header while typing, drops it on blur", async () => {
+        const user = userEvent.setup();
+        const enqueue = jest.fn();
+        renderWithProps({ enqueue });
+        const input = screen.getByRole('textbox');
+        await user.type(input, 'a');
+        const changingHeader = { "x-r-changing": "1" };
+        expect(enqueue).toHaveBeenLastCalledWith(
+            expect.anything(),
+            expect.objectContaining({ headers: expect.objectContaining(changingHeader) })
+        );
+        await user.tab();
+        expect(enqueue).toHaveBeenLastCalledWith(
+            expect.anything(),
+            expect.not.objectContaining({ headers: expect.objectContaining(changingHeader) })
+        );
+        expect(enqueue).toHaveBeenCalledTimes(2);
     });
 });
