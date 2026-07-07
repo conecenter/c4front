6a58c3d5d9842a27fcb334556281869bbf1ff65d
Aleafan <aaf@cone.ee>
2026-07-07 15:52:22 +0200

    update FocusAnnouncerApi to use ProtocolReceiver and add FocusChanged message

--- src/sapi/ee/cone/c4ui/FocusAnnouncerApi.scala
+++ src/sapi/ee/cone/c4ui/FocusAnnouncerApi.scala
@@ -1,14 +1,18 @@
 package ee.cone.c4ui
 
 import ee.cone.c4vdom.Types.ViewRes
-import ee.cone.c4vdom.{Receiver, ToChildPair, c4elPath, c4tags}
+import ee.cone.c4vdom.{ProtocolReceiver, ToChildPair, c4elPath, c4msg, c4msgSwitch, c4tags}
 
+@c4msgSwitch("FrontApp") sealed trait FocusAnnouncerMsg
+
+@c4msg(action = "change", body = "focusPath")
+final case class FocusChanged(focusPath: String) extends FocusAnnouncerMsg
 
 @c4tags("FrontApp") trait FocusAnnouncerTags[C] {
   @c4elPath("FocusAnnouncerElement") def focusAnnouncer(
     key: String,
     value: String,
-    receiver: Receiver[C],
+    receiver: ProtocolReceiver[C, FocusAnnouncerMsg],
     children: ViewRes,
   ): ToChildPair
 }
